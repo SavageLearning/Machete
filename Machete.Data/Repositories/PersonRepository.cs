159bb8d821dd042f1d73d91ac9a4a3949ff62764
jimmy <jimmy@4c19f846-1fea-3644-b4c7-c84c78303d7d>
2011-02-17 06:40:53 +0000

    Added PersonRepository definition

---
+++ Machete.Data/Repositories/PersonRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Machete.Domain;
+using Machete.Data.Infrastructure;
+
+namespace Machete.Data
+{
+    public class PersonRepository: RepositoryBase<Person>, IPersonRepository
+    {
+        public PersonRepository(IDatabaseFactory databaseFactory)
+            : base(databaseFactory)
+        {
+        }
+    }
+    public interface IPersonRepository : IRepository<Person>
+    {
+    }
+}
