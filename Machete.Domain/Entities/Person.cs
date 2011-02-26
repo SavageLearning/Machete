1840435ad1e9c08966fafa3c3b78edeca980c60e
jimmy <jimmy@4c19f846-1fea-3644-b4c7-c84c78303d7d>
2011-02-26 02:50:46 +0000

    Person.cs: Localized Stringlength messages, added more RequiredAttributes AccountModels.cs: Localized requiredattribute error messages

--- Machete.Domain/Entities/Person.cs
+++ Machete.Domain/Entities/Person.cs
@@ -13,39 +13,59 @@ namespace Machete.Domain
 
     public class Person
     {
-        //TODO: 1: Finish restrictions on Person
         public int ID { get; set; }
+        //
         [LocalizedDisplayName("firstname1", NameResourceType = typeof(Persons))]
-        [StringLength(50)]
+        [StringLength(50, ErrorMessageResourceName = "stringlength", ErrorMessageResourceType = typeof(Persons))]
         [Required(ErrorMessageResourceName = "firstname1error", ErrorMessageResourceType = typeof(Persons))]
         public string firstname1 { get; set; }
+        //
         [LocalizedDisplayName("firstname2", NameResourceType = typeof(Persons))]
-        [StringLength(50)]
+        [StringLength(50, ErrorMessageResourceName = "stringlength", ErrorMessageResourceType = typeof(Persons))]
         public string firstname2 { get; set; }
+        //
         [LocalizedDisplayName("lastname1", NameResourceType = typeof(Persons))]
-        [StringLength(50)]
+        [StringLength(50, ErrorMessageResourceName = "stringlength", ErrorMessageResourceType = typeof(Persons))]
         [Required(ErrorMessageResourceName = "lastname1error", ErrorMessageResourceType = typeof(Persons))]
         public string lastname1 { get; set; }
+        //
         [LocalizedDisplayName("lastname2", NameResourceType = typeof(Persons))]
-        [StringLength(50)]
+        [StringLength(50, ErrorMessageResourceName = "stringlength", ErrorMessageResourceType = typeof(Persons))]
         public string lastname2 { get; set; }
+        //
         [LocalizedDisplayName("address1", NameResourceType = typeof(Persons))]
+        [StringLength(50, ErrorMessageResourceName = "stringlength", ErrorMessageResourceType = typeof(Persons))]
         public string address1 { get; set; }
+        //
         [LocalizedDisplayName("address2", NameResourceType = typeof(Persons))]
+        [StringLength(50, ErrorMessageResourceName = "stringlength", ErrorMessageResourceType = typeof(Persons))]
         public string address2 { get; set; }
+        //
         [LocalizedDisplayName("city", NameResourceType = typeof(Persons))]
+        [StringLength(25, ErrorMessageResourceName = "stringlength", ErrorMessageResourceType = typeof(Persons))]
         public string city { get; set; }
+        //
         [LocalizedDisplayName("state", NameResourceType = typeof(Persons))]
+        [StringLength(2, ErrorMessageResourceName = "stringlength", ErrorMessageResourceType = typeof(Persons))]
         public string state { get; set; }
+        //
         [LocalizedDisplayName("zipcode", NameResourceType = typeof(Persons))]
+        [StringLength(10, ErrorMessageResourceName = "stringlength", ErrorMessageResourceType = typeof(Persons))]
         public string zipcode { get; set; }
+        //
         [LocalizedDisplayName("phone", NameResourceType = typeof(Persons))]
+        [StringLength(12, ErrorMessageResourceName = "stringlength", ErrorMessageResourceType = typeof(Persons))]
         public string phone { get; set; }
+        // TODO: GUI drop down & limit gender characters
         [LocalizedDisplayName("gender", NameResourceType = typeof(Persons))]
+        [StringLength(1, ErrorMessageResourceName = "stringlength", ErrorMessageResourceType = typeof(Persons))]
+        [Required(ErrorMessageResourceName = "gendererror", ErrorMessageResourceType = typeof(Persons))]
         public string gender { get; set; }
+        //
         [LocalizedDisplayName("genderother", NameResourceType = typeof(Persons))]
+        [StringLength(20, ErrorMessageResourceName = "stringlength", ErrorMessageResourceType = typeof(Persons))]
         public string genderother { get; set; }
-        // TODO: Have PersonController update time settings
+
         public DateTime datecreated { get; set; }
         public DateTime dateupdated { get; set; }
     }
