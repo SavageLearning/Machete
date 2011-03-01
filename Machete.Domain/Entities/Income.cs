b53ad5e11df5565a15266e0968b951d588240afc
Jimmy Carter <jciispam@gmail.com>
2011-03-01 04:01:56 +0000

    Added Entities for future use

---
+++ Machete.Domain/Entities/Income.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Machete.Domain.Resources;
+
+namespace Machete.Domain
+{
+    public class Income
+    {
+        public byte ID { get; set; }
+        public string incomelabel { get; set; }
+    }
+}
