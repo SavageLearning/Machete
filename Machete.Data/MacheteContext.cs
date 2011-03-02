0a836c192beeafc2a3543a3c23f6d06e8a18fa6b
jimmy <jimmy@4c19f846-1fea-3644-b4c7-c84c78303d7d>
2011-03-02 20:56:56 +0000

    Machete.Data updaates:     MacheteInitializer.cs: Controls database initialization. sets up lookup data.     MacheteContext.cs: Name changes, base->machete to protect mote work, commented out tables until ready to use

--- Machete.Data/MacheteContext.cs
+++ Machete.Data/MacheteContext.cs
@@ -9,25 +9,27 @@ namespace Machete.Data
 {
     public class MacheteContext : DbContext
     {
-        public MacheteContext() : base("mote") { } //Machete here defines the database to use, by convention.
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Expense> Expenses { get; set; }
+        public MacheteContext() : base("machete") { } //Machete here defines the database to use, by convention.
+        //public DbSet<Category> Categories { get; set; }
+        //public DbSet<Expense> Expenses { get; set; }
         public DbSet<Person> Persons { get; set; }
         public DbSet<Worker> Workers { get; set; }
-        public DbSet<Race> LRaces { get; set; }
-        public DbSet<EnglishLevel> LEnglishLevel { get; set; }
-        public DbSet<Income> LIncome { get; set; }
-        public DbSet<Neighborhood> LNeighborhood { get; set; }
-        public DbSet<SkillLevel> LSkillLevel { get; set; }
-        public DbSet<WorkerSignin> WorkerSignins { get; set; }
-        public DbSet<Employer> Employers { get; set; }
-        public DbSet<WorkOrder> WorkOrders { get; set; }
-        public DbSet<WorkerSkill> WorkerSkills { get; set; }
-        public DbSet<WorkAssignment> WorkAssignments { get; set; }
-        public DbSet<Survey> Surveys { get; set; }
+        public DbSet<Race> Races { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Neighborhood> Neighborhoods { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        //public DbSet<WorkerSignin> WorkerSignins { get; set; }
+        //public DbSet<Employer> Employers { get; set; }
+        //public DbSet<WorkOrder> WorkOrders { get; set; }
+        //public DbSet<WorkerSkill> WorkerSkills { get; set; }
+        //public DbSet<WorkAssignment> WorkAssignments { get; set; }
+        //public DbSet<Survey> Surveys { get; set; }
 
         public virtual void Commit()
         {
+            //TODO: Catch and handle database exceptions
+            
             base.SaveChanges();
         }
 
@@ -35,19 +37,19 @@ namespace Machete.Data
         {
             base.OnModelCreating(modelBuilder);
             
-            modelBuilder.Entity<Person>().ToTable("persons");
-            modelBuilder.Entity<Worker>().ToTable("WorkerInfo");
-            modelBuilder.Entity<Race>().ToTable("LookupRace");
-            modelBuilder.Entity<EnglishLevel>().ToTable("LookupEnglishlevel");
-            modelBuilder.Entity<Income>().ToTable("LookupIncome");
-            modelBuilder.Entity<SkillLevel>().ToTable("LookupSkillLevel");
-            modelBuilder.Entity<Neighborhood>().ToTable("LookupNeighborhood");
-            modelBuilder.Entity<WorkerSignin>().ToTable("WorkerSignin");
-            modelBuilder.Entity<Employer>().ToTable("Employers");
-            modelBuilder.Entity<WorkOrder>().ToTable("WorkOrders");
-            modelBuilder.Entity<WorkerSkill>().ToTable("WorkerSkills");
-            modelBuilder.Entity<WorkAssignment>().ToTable("WorkAssignments");
-            modelBuilder.Entity<Survey>().ToTable("Surveys");
+            //modelBuilder.Entity<Person>().ToTable("persons");
+            //modelBuilder.Entity<Worker>().ToTable("WorkerInfo");
+            //modelBuilder.Entity<Race>().ToTable("LookupRace");
+            //modelBuilder.Entity<EnglishLevel>().ToTable("LookupEnglishlevel");
+            //modelBuilder.Entity<Income>().ToTable("LookupIncome");
+            //modelBuilder.Entity<SkillLevel>().ToTable("LookupSkillLevel");
+            //modelBuilder.Entity<Neighborhood>().ToTable("LookupNeighborhood");
+            //modelBuilder.Entity<WorkerSignin>().ToTable("WorkerSignin");
+            //modelBuilder.Entity<Employer>().ToTable("Employers");
+            //modelBuilder.Entity<WorkOrder>().ToTable("WorkOrders");
+            //modelBuilder.Entity<WorkerSkill>().ToTable("WorkerSkills");
+            //modelBuilder.Entity<WorkAssignment>().ToTable("WorkAssignments");
+            //modelBuilder.Entity<Survey>().ToTable("Surveys");
 
         }
     }
