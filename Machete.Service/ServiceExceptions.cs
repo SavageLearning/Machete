459eb923d51e4a9f6fc2176bfe555e3d8cbbecaf
jimmy <jimmy@4c19f846-1fea-3644-b4c7-c84c78303d7d>
2011-03-05 07:56:08 +0000

    TortoiseSVN fixes Ankh's locking problems. Awesome.

---
+++ Machete.Service/ServiceExceptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Machete.Service
+{
+    [Serializable()]
+    public class MissingReferenceException : System.Exception
+    {
+        public MissingReferenceException() : base() { }
+        public MissingReferenceException(string message) : base(message) { }
+        public MissingReferenceException(string message, System.Exception inner) : base(message, inner) { }
+
+        // A constructor is needed for serialization when an
+        // exception propagates from a remoting server to the client. 
+        protected MissingReferenceException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) { }
+    }
+}
