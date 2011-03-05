eadc65e87d28dae8f87de593dd72f08730cb5c76
Jimmy Carter <jciispam@gmail.com>
2011-03-05 06:15:49 +0000

    Machete.Test:     First round of unit testing.

--- Machete.Test/Domain/WorkerTests.cs
+++ Machete.Test/Domain/WorkerTests.cs
@@ -8,46 +8,104 @@ using Machete.Data;
 using Machete.Data.Infrastructure;
 using System.Data.Entity.Database;
 using System.Data.Entity.Infrastructure;
+using Machete.Service;
 namespace Machete.Test
 {
     [TestClass]
     public class WorkerTests
     {
+        MacheteContext MacheteDB;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            DbDatabase.SetInitializer<MacheteContext>(new MacheteInitializer());
+            this.MacheteDB = new MacheteContext();
+        }
+        
         [TestMethod]
         public void Worker_add_simple_record()
         {
             //
             //Arrange
-            DbDatabase.SetInitializer<MacheteContext>(new MacheteInitializer());
-            MacheteContext MacheteDB = new MacheteContext();
+            MacheteDB.Database.Delete();
+            MacheteDB.Database.Initialize(true);
+            Person _person1 = Records._person1;
+            Person _person2 = Records._person2;
+            Person _person3 = Records._person3;
+            Worker _worker1 = Records._worker1;
+            Worker _worker2 = Records._worker2;
+            _person1.firstname2 = "Worker_add_simple_record";
+            _person2.firstname2 = "Worker_add_simple_record";
+            _person2.Worker = _worker2;
+            _person3.firstname2 = "Worker_add_simple_record";
+            _worker1.height = "Worker_add_simple_record";
+            _worker2.height = "Worker_add_simple_record";
+            MacheteDB.Persons.Add(_person1);
+            MacheteDB.Persons.Add(_person3);
             //
             //Act
-            try
-            {
-                Records._person2.Worker = Records._worker2;
-                MacheteDB.Persons.Add(Records._person1);
-                MacheteDB.Persons.Add(Records._person3);
+            //try
+            //{
                 MacheteDB.SaveChanges();
-                MacheteDB.Persons.Add(Records._person2);
+                MacheteDB.Persons.Add(_person2);
                 MacheteDB.SaveChanges();
-                Records._person1.Worker = Records._worker1;
-                MacheteDB.Workers.Add(Records._worker1);
+                _worker1.Person = _person1;
+                MacheteDB.Workers.Add(_worker1);
                 //TODO: DBUpdat Exception here. Add a record after already added for error.
                 MacheteDB.SaveChanges();
-            }
-            catch (Exception e)
-            {
-                Assert.Fail(string.Format("Unexpected exception of type {0} caught: {1}",
-                e.GetType(), e.Message));
-            }
+
+            //}
+            //catch (Exception e)
+            //{
+            //    Assert.Fail(string.Format("Unexpected exception of type {0} caught: {1}",
+            //    e.GetType(), e.Message));
+            //}
             //
             //Assert
-            Assert.IsTrue(Records._person1.ID == 1);
-            Assert.IsTrue(Records._person2.ID == 3);
-            Assert.IsNotNull(Records._person1.Worker);
-            Assert.IsNotNull(Records._person2.Worker);
-            Assert.IsNull(Records._person3.Worker);
-            Assert.IsInstanceOfType(Records._worker1.Person, typeof(Person));
+            Assert.IsTrue(_person1.ID == 1);
+            Assert.IsTrue(_person2.ID == 3);
+            Assert.IsNotNull(_person1.Worker);
+            Assert.IsNotNull(_person2.Worker);
+            Assert.IsNull(_person3.Worker);
+            Assert.IsInstanceOfType(_worker1.Person, typeof(Person));
+        }
+        [TestMethod]
+        public void Worker_test_deduplication()
+        {
+            int reccount = 0;
+            //
+            //Arrange
+            MacheteDB.Database.Delete();
+            MacheteDB.Database.Initialize(true);
+            //MacheteDB.Configuration.AutoDetectChangesEnabled = true;
+            Person _person2 = Records._person2;
+            Worker _worker2 = Records._worker2;
+            _person2.firstname2 = "Worker_test_deduplication";
+            _worker2.height = "Worker_test_deduplication";
+            _worker2.Person = _person2;
+            //
+            //Act
+            //try
+            //{
+                MacheteDB.Workers.Add(_worker2);
+                MacheteDB.SaveChanges();
+                MacheteDB.Commit();
+                MacheteDB.Persons.Add(_person2);
+                MacheteDB.SaveChanges();
+                MacheteDB.Persons.Add(_person2);
+                MacheteDB.SaveChanges();
+                reccount = MacheteDB.Persons.Count(n => n.firstname1 == _person2.firstname1);
+            //}
+            //catch (Exception e)
+            //{
+            //    Assert.Fail(string.Format("Unexpected exception of type {0} caught: {1}",
+            //    e.GetType(), e.Message));
+            //}
+            //
+            //Assert
+            Assert.IsTrue(reccount == 1, "Deduplication of records failed.");
+
         }
     }
 }
---
+++ Machete.Test/Services/WorkerServiceTest.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Machete.Data;
+using Machete.Data.Infrastructure;
+using Machete.Domain;
+using Machete.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Entity.Database;
+
+namespace Machete.Test
+{
+    [TestClass]
+    public class WorkerServiceTest
+    {
+        WorkerRepository _workerRepo;
+        DatabaseFactory _dbFactory;
+        WorkerService _service;
+        IUnitOfWork _unitofwork;
+        MacheteContext MacheteDB;
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            //DbDatabase.SetInitializer<MacheteContext>(new MacheteInitializer());
+        }
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            DbDatabase.SetInitializer<MacheteContext>(new MacheteInitializer());
+            this.MacheteDB = new MacheteContext();
+            _dbFactory = new DatabaseFactory();
+            _workerRepo = new WorkerRepository(_dbFactory);
+            _unitofwork = new UnitOfWork(_dbFactory);
+            _service = new WorkerService(_workerRepo, _unitofwork);
+        }
+
+        [TestMethod]
+        public void WorkerService_Intergation_CreateWorker()
+        {
+            //
+            //Arrange
+            MacheteDB.Database.Delete();
+            MacheteDB.Database.Initialize(true);
+            Worker _worker3 = Records._worker3;
+            Person _person3 = Records._person3;
+            _person3.firstname2 = "WorkerService_Intergation_CreateWorker";
+            _worker3.height = "WorkerService_Intergation_CreateWorker";
+            _worker3.Person = _person3;
+            //
+            //Act
+            _service.CreateWorker(_worker3);
+
+            //
+            //Assert
+            Assert.IsNotNull(_worker3.ID, "Worker.ID is Null");
+            //Assert.IsTrue(Records._person3.ID == 1, "Record did not have expected ID");
+            Assert.IsTrue(_worker3.ID == _person3.ID, "Worker.ID doesn't match Person.ID");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(MissingReferenceException))]
+        public void WorkerService_Intergation_Detect_Missing_Person_Reference()
+        {
+            //
+            //Arrange
+            MacheteDB.Database.Delete();
+            MacheteDB.Database.Initialize(true);
+            Person _person2 = Records._person2;
+            Worker _worker2 = Records._worker2;
+            _person2.Worker = _worker2;
+            _person2.firstname2 = "WorkerService_Intergation_Detect_Missing_Person_Reference";
+            _worker2.height = "WorkerService_Intergation_Detect_Missing_Person_Reference";
+            //
+            //Act
+            _service.CreateWorker(_worker2);
+            //
+            //Assert
+        }
+        [TestMethod]
+        public void WorkerService_Intergation_CreateWorkers_NoDuplicate()
+        {
+            int reccount = 0;
+            //
+            //Arrange
+            MacheteDB.Database.Delete();
+            MacheteDB.Database.Initialize(true);
+            Worker _worker3 = Records._worker3;
+            Person _person3 = Records._person3;
+            _person3.firstname2 = "WorkerService_Intergation_CreateWorkers_NoDuplicate";
+            _worker3.height = "WorkerService_Intergation_CreateWorkers_NoDuplicate";
+ 
+            if (_worker3.Person  == null) 
+                { _worker3.Person = _person3;}
+            //
+            //Act          
+            _service.CreateWorker(_worker3);
+            _service.CreateWorker(_worker3);
+            _service.CreateWorker(_worker3);
+            reccount = MacheteDB.Workers.Count(n => n.raceother == _worker3.raceother);
+            //
+            //Assert
+            //TODO: figure out why de-dup isn't working
+            Assert.IsNotNull(_worker3.ID);
+            Assert.IsTrue(reccount == 1);
+        }
+    }
+}
