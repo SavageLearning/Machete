c027c2fd6a1d9c0b3e0796e49bc8155de36d8c5f
jimmy <jimmy@4c19f846-1fea-3644-b4c7-c84c78303d7d>
2011-03-05 06:15:49 +0000

    Machete.Test:     First round of unit testing.

--- Machete.Test/Records.cs
+++ Machete.Test/Records.cs
@@ -82,9 +82,47 @@ namespace Machete.Test
             Createdby = Guid.Empty,
             Updatedby = Guid.Empty
         };
+        public static Worker _worker3 = new Worker
+        {                                   //#C data type
+            RaceID = 1,                     //byte
+            raceother = "Records._worker3",         //string
+            height = "too tall",            //string
+            weight = "too big",             //string
+            englishlevelID = 1,             //byte
+            recentarrival = true,           //bool
+            dateinUSA = DateTime.Now,       //datetime
+            dateinseattle = DateTime.Now,   //datetime
+            disabled = true,                //bool
+            disabilitydesc = "foo",         //string
+            maritalstatus = "S",            //string
+            livewithchildren = true,        //bool
+            numofchildren = 0,              //byte
+            incomeID = 1,                   //byte
+            livealone = true,               //bool
+            emcontUSAname = "Bill Clinton", //string
+            emcontUSAphone = "1234567890",  //string
+            emcontUSArelation = "idol",     //string
+            dwccardnum = 1,                 //int
+            neighborhoodID = 1,             //byte
+            immigrantrefugee = true,        //bool
+            countryoforigin = "USA",        //string
+            emcontoriginname = "Barak Obama",   //string
+            emcontoriginphone = "1234567890",   //string
+            emcontoriginrelation = "friend",    //string
+            memberexpirationdate = DateTime.Now,    //datetime
+            driverslicense = true,                  //bool
+            licenseexpirationdate = DateTime.Now,   //datetime
+            carinsurance = true,                    //bool
+            insuranceexpiration = DateTime.Now,     //datetime
+            datecreated = DateTime.Now,             //datetime
+            dateupdated = DateTime.Now,              //datetime
+            Createdby = Guid.Empty,
+            Updatedby = Guid.Empty
+        };
         public static Person _person1 = new Person
         {
             firstname1 = "Records._person1",
+            firstname2 = "default record",
             lastname1 = "testestestest",
             address1 = "123 Foo St",
             city = "Foo town",
@@ -100,6 +138,7 @@ namespace Machete.Test
         public static Person _person2 = new Person
         {
             firstname1 = "Records._person2",
+            firstname2 = "default record",
             lastname1 = "testestestest",
             address1 = "123 Foo St",
             city = "Foo town",
@@ -115,6 +154,7 @@ namespace Machete.Test
         public static Person _person3 = new Person
         {
             firstname1 = "Records._person3",
+            firstname2 = "default record",
             lastname1 = "testestestest",
             address1 = "123 Foo St",
             city = "Foo town",
@@ -127,5 +167,38 @@ namespace Machete.Test
             Createdby = Guid.Empty,
             Updatedby = Guid.Empty
         };
+        public static Person _person4 = new Person
+        {
+            firstname1 = "Records._person4",
+            firstname2 = "default record",
+            lastname1 = "testestestest",
+            address1 = "123 Foo St",
+            city = "Foo town",
+            state = "FO",
+            zipcode = "12345",
+            phone = "No phone",
+            gender = "T",
+            datecreated = DateTime.Now,             //datetime
+            dateupdated = DateTime.Now,              //datetime
+            Createdby = Guid.Empty,
+            Updatedby = Guid.Empty
+        };
+        public static Person _person5 = new Person
+        {
+            firstname1 = "Records._person5",
+            firstname2 = "default record",
+            lastname1 = "testestestest",
+            address1 = "123 Foo St",
+            city = "Foo town",
+            state = "FO",
+            zipcode = "12345",
+            phone = "No phone",
+            gender = "T",
+            datecreated = DateTime.Now,             //datetime
+            dateupdated = DateTime.Now,              //datetime
+            Createdby = Guid.Empty,
+            Updatedby = Guid.Empty
+        };
+
     }
 }
---
+++ Machete.Test/Services/PersonServiceTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Machete.Data;
+using Machete.Data.Infrastructure;
+using Machete.Domain;
+using Machete.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Entity.Database;
+
+namespace Machete.Test
+{
+    [TestClass]
+    public class PersonServiceTest
+    {
+        PersonRepository _personRepo;
+        DatabaseFactory _dbFactory;
+        PersonService _service;
+        IUnitOfWork _unitofwork;
+        MacheteContext MacheteDB;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            //DbDatabase.SetInitializer<MacheteContext>(new MacheteInitializer());
+        }
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+
+            DbDatabase.SetInitializer<MacheteContext>(new MacheteInitializer());
+            this.MacheteDB = new MacheteContext();
+            _dbFactory = new DatabaseFactory();
+            _personRepo = new PersonRepository(_dbFactory);
+            _unitofwork = new UnitOfWork(_dbFactory);
+            _service = new PersonService(_personRepo, _unitofwork);
+        }
+
+        [TestMethod]
+        public void PersonService_Intergation_CreatePerson()
+        {
+
+            //
+            //Arrange
+            MacheteDB.Database.Delete();
+            MacheteDB.Database.Initialize(true);
+            Person _person = Records._person4;
+            _person.firstname2 = "PersonService_Intergation_CreatePerson";
+
+            //
+            //Act
+            _service.CreatePerson(_person);
+
+            //
+            //Assert
+            Assert.IsNotNull(_person.ID, "Person.ID is Null");
+            Assert.IsTrue(_person.ID == 1);
+        }
+        [TestMethod]
+        public void PersonService_Intergation_CreatePersons_NoDuplicate()
+        {
+            int reccount = 0;
+            //
+            //Arrange
+            MacheteDB.Database.Delete();
+            MacheteDB.Database.Initialize(true);
+            Person _person4 = Records._person4;
+            _person4.firstname2 = "PersonService_Intergation_CreatePersons_NoDuplicate";
+            //
+            //Act          
+            _service.CreatePerson(_person4);
+            _service.CreatePerson(_person4);
+            _service.CreatePerson(_person4);
+            reccount = MacheteDB.Persons.Count(n => n.firstname1 == _person4.firstname1);
+            //
+            //Assert
+            //TODO: figure out why de-dup isn't working
+            Assert.IsNotNull(_person4.ID);
+            Assert.IsTrue(reccount == 1);
+      
+        }
+    }
+}
