6672bff49c0cd3c23e1aa8f5b33b29f37f90e863
Jimmy Carter <jciispam@gmail.com>
2011-03-14 07:10:22 +0000

    Added ErrorController Added Http404 page Deleted more files from starter template

--- Machete.Data/MacheteContext.cs
+++ Machete.Data/MacheteContext.cs
@@ -11,8 +11,6 @@ namespace Machete.Data
     public class MacheteContext : DbContext
     {
         public MacheteContext() : base("macheteConnection") { } //Machete here defines the database to use, by convention.
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Expense> Expenses { get; set; }
         public DbSet<Person> Persons { get; set; }
         public DbSet<Worker> Workers { get; set; }
         public DbSet<Race> Races { get; set; }
---
+++ Machete.Web/Controllers/ErrorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Machete.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        //
+        // GET: /Error/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Http404(string url)
+        {
+            Response.StatusCode = 404;
+            ViewData["url"] = url;
+            return View();
+        }
+
+
+    }
+}
