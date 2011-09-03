e69bbfc3a5d6a68c82081873d60b08f48fc3902f
Jimmy Carter <jciispam@gmail.com>
2011-09-03 05:09:01 +0000

    WorkerSignin Requested filter works

--- Machete.Data/Initialize/MacheteInitializer.cs
+++ Machete.Data/Initialize/MacheteInitializer.cs
@@ -16,7 +16,7 @@ namespace Machete.Data
             //Initialize Lookups
             MacheteLookup.Initialize(DB);
             DB.SaveChanges();
-            
+            DB.Database.SqlCommand("CREATE NONCLUSTERED INDEX [dateTimeofWork] ON [dbo].[WorkOrders] ([dateTimeofWork] ASC) WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]");
             new List<Person>
             {
                 new Person { firstname1 = "Jimmy", 
--- Machete.Data/Initialize/TestInitializer.cs
+++ Machete.Data/Initialize/TestInitializer.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Data.Entity.Database;
 using Machete.Domain;
+using System.Collections.ObjectModel;
 
 namespace Machete.Data
 {
@@ -13,8 +14,8 @@ namespace Machete.Data
         {
             //Initialize Lookup tables with static data
             MacheteLookup.Initialize(DB);
-            DB.SaveChanges(); 
-            
+            DB.SaveChanges();
+
             Person person = new Person
             {
                 firstname1 = "barack",
@@ -30,7 +31,7 @@ namespace Machete.Data
             {
                 RaceID = MacheteLookup.getID(DB, "race", "Latino"),
                 active = true,
-                typeOfWorkID=20,
+                typeOfWorkID = 20,
                 dateOfMembership = DateTime.Now,
                 dateOfBirth = DateTime.Now,
                 height = "6ft 1in",
@@ -118,42 +119,66 @@ namespace Machete.Data
                 Createdby = "initialization script",
                 Updatedby = "initialization script"
             };
+            WorkerRequest request = new WorkerRequest
+            {
+                datecreated = DateTime.Now,             //datetime
+                dateupdated = DateTime.Now,              //datetime
+                Createdby = "initialization script",
+                Updatedby = "initialization script"
+            };
             var dt = DateTime.Parse("8/10/2011");
             Person p1 = (Person)person.Clone(); DB.Persons.Add(p1); p1.Worker = (Worker)worker.Clone(); p1.Worker.dwccardnum = 30040; p1.Worker.skill1 = 62;
             Person p2 = (Person)person.Clone(); DB.Persons.Add(p2); p2.Worker = (Worker)worker.Clone(); p2.Worker.dwccardnum = 30041;
             Person p3 = (Person)person.Clone(); DB.Persons.Add(p3); p3.Worker = (Worker)worker.Clone(); p3.Worker.dwccardnum = 30042;
             DB.SaveChanges();
-            WorkerSignin wsi1 = (WorkerSignin)signin.Clone(); DB.WorkerSignins.Add(wsi1); wsi1.dwccardnum = 30040; wsi1.dateforsignin = dt;
-            WorkerSignin wsi2 = (WorkerSignin)signin.Clone(); DB.WorkerSignins.Add(wsi2); wsi2.dwccardnum = 30041; wsi2.dateforsignin = dt;
-            WorkerSignin wsi3 = (WorkerSignin)signin.Clone(); DB.WorkerSignins.Add(wsi3); wsi3.dwccardnum = 30042; wsi3.dateforsignin = dt;
+            WorkerSignin wsi1 = (WorkerSignin)signin.Clone(); DB.WorkerSignins.Add(wsi1); wsi1.dwccardnum = 30040; wsi1.dateforsignin = dt; wsi1.WorkerID = 1;
+            WorkerSignin wsi2 = (WorkerSignin)signin.Clone(); DB.WorkerSignins.Add(wsi2); wsi2.dwccardnum = 30041; wsi2.dateforsignin = dt; wsi2.WorkerID = 2;
+            WorkerSignin wsi3 = (WorkerSignin)signin.Clone(); DB.WorkerSignins.Add(wsi3); wsi3.dwccardnum = 30042; wsi3.dateforsignin = dt; wsi3.WorkerID = 3;
             WorkerSignin wsi4 = (WorkerSignin)signin.Clone(); DB.WorkerSignins.Add(wsi4); wsi4.dwccardnum = 30043; wsi4.dateforsignin = dt;
-            WorkerSignin wsi5 = (WorkerSignin)signin.Clone(); DB.WorkerSignins.Add(wsi5); wsi5.dwccardnum = 30044; wsi5.dateforsignin = dt;
+            WorkerSignin wsi5 = (WorkerSignin)signin.Clone(); DB.WorkerSignins.Add(wsi5); wsi5.dwccardnum = 30044; wsi5.dateforsignin = dt; 
             DB.SaveChanges();
             Employer em1 = (Employer)employer.Clone(); DB.Employers.Add(em1);
             Employer em2 = (Employer)employer.Clone(); DB.Employers.Add(em2);
             Employer em3 = (Employer)employer.Clone(); DB.Employers.Add(em3);
             DB.SaveChanges();
-            WorkOrder em1od1 = (WorkOrder)order.Clone(); em1od1.Employer = em1; em1od1.paperOrderNum = 12420; em1od1.contactName = "Umpa1"; em1od1.status = 42; DB.WorkOrders.Add(em1od1);// em1.WorkOrders.Add(em1od1);
+            
+                WorkOrder em1od1 = (WorkOrder)order.Clone();
+                em1od1.Employer = em1;
+                em1od1.paperOrderNum = 12420;
+                em1od1.contactName = "Umpa1";
+                em1od1.status = 42;
+                WorkerRequest od1wr1 = (WorkerRequest)request.Clone();
+                
+                DB.WorkOrders.Add(em1od1); //CreateWorkOrder
+                em1od1.workerRequests = new Collection<WorkerRequest>();
+                DB.SaveChanges();
+                od1wr1.workerRequested = p2.Worker;
+                od1wr1.workOrder = em1od1;
+
+
+                em1od1.workerRequests.Add(od1wr1);
+                // em1.WorkOrders.Add(em1od1);
+                DB.SaveChanges();
             WorkOrder em2od1 = (WorkOrder)order.Clone(); em2od1.Employer = em2; em2od1.paperOrderNum = 12421; em2od1.contactName = "Umpa2"; em2od1.status = 43; DB.WorkOrders.Add(em2od1);
             WorkOrder em2od2 = (WorkOrder)order.Clone(); em2od2.Employer = em2; em2od2.paperOrderNum = 12422; em2od2.contactName = "Umpa3"; em2od2.status = 44; DB.WorkOrders.Add(em2od2);
             WorkOrder em3od1 = (WorkOrder)order.Clone(); em3od1.Employer = em3; em3od1.paperOrderNum = 12423; em3od1.contactName = "Umpa4"; em3od1.status = 45; DB.WorkOrders.Add(em3od1);
             WorkOrder em3od2 = (WorkOrder)order.Clone(); em3od2.Employer = em3; em3od2.paperOrderNum = 12424; em3od2.contactName = "Umpa5"; em3od2.status = 44; DB.WorkOrders.Add(em3od2);
             WorkOrder em3od3 = (WorkOrder)order.Clone(); em3od3.Employer = em3; em3od3.paperOrderNum = 12425; em3od3.contactName = "Umpa6"; em3od3.status = 42; DB.WorkOrders.Add(em3od3);
             DB.SaveChanges();
-            WorkAssignment em1od1as1 = (WorkAssignment)assignment.Clone(); em1od1as1.workOrder = em1od1; 
-                em1od1as1.description = "foostring1"; 
-                em1od1as1.Updatedby = "foostring2";
-                em1od1as1.workerAssignedID = 1;
-                em1od1as1.skillID = 70;  DB.WorkAssignments.Add(em1od1as1);
-            
-                em1od1.dateTimeofWork = DateTime.Parse("08-10-2011 09:00:00");
+            WorkAssignment em1od1as1 = (WorkAssignment)assignment.Clone(); em1od1as1.workOrder = em1od1;
+            em1od1as1.description = "foostring1";
+            em1od1as1.Updatedby = "foostring2";
+            em1od1as1.workerAssignedID = 1;
+            em1od1as1.skillID = 70; DB.WorkAssignments.Add(em1od1as1);
+
+            em1od1.dateTimeofWork = DateTime.Parse("08-10-2011 09:00:00");
             WorkAssignment em1od1as2 = (WorkAssignment)assignment.Clone(); em1od1as2.workOrder = em1od1; DB.WorkAssignments.Add(em1od1as2);
-                em1od1as2.workerAssignedID = 2;
-                em1od1as2.skillID = 61; 
-                em1od1as2.englishLevelID = 2;
+            em1od1as2.workerAssignedID = 2;
+            em1od1as2.skillID = 61;
+            em1od1as2.englishLevelID = 2;
             WorkAssignment em1od1as3 = (WorkAssignment)assignment.Clone(); em1od1as3.workOrder = em1od1; DB.WorkAssignments.Add(em1od1as3);
-                em1od1as1.workerAssignedID = 1;
-                em1od1as3.workerAssignedID = 3;
+            em1od1as1.workerAssignedID = 1;
+            em1od1as3.workerAssignedID = 3;
             //
             WorkAssignment em2od1as1 = (WorkAssignment)assignment.Clone(); em2od1as1.workOrder = em2od1; DB.WorkAssignments.Add(em2od1as1);
             em2od1as1.workerAssignedID = 1;
@@ -172,7 +197,7 @@ namespace Machete.Data
             em3od2as1.workerAssignedID = 1;
             //
             WorkAssignment em3od3as1 = (WorkAssignment)assignment.Clone(); em3od3as1.workOrder = em3od3; DB.WorkAssignments.Add(em3od3as1);
-            
+
             DB.SaveChanges();
         }
     }
