3c898ef5b5337178d0b452f187943dd34493c806
Jimmy Carter <jciispam@gmail.com>
2011-11-10 21:18:14 +0000

    Added Reports controller. Simple page to serve links to the SSRS server

---
+++ Machete.Web/Controllers/ReportsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Machete.Helpers;
+using Elmah;
+
+namespace Machete.Web.Controllers
+{
+    [ElmahHandleError]
+    public class ReportsController : Controller
+    {
+        //
+        // GET: /Reports/
+        [Authorize(Roles = "Administrator, Manager")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
