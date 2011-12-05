e7f8a9aae087b2e2662755ef5fdf741b43ed282e
jimmy <jimmy@4c19f846-1fea-3644-b4c7-c84c78303d7d>
2011-12-05 17:54:26 +0000

    PersonTests.cs: Selenium WebDriver working example Added #personSearchBox to Datatable search box (for testing) still need to figure out why employer move/record save is slow

--- Machete.Test/IntegrationTests/Views/PersonTests.cs
+++ Machete.Test/IntegrationTests/Views/PersonTests.cs
@@ -1,117 +1,180 @@
-﻿//using System;
-//using System.Text;
-//using System.Text.RegularExpressions;
-//using System.Threading;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Selenium;
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
 
-//namespace Machete.Test
-//{
-//    [TestClass]
-//    public class MacheteSignin
-//        {
-//        private ISelenium selenium;
-//        private StringBuilder verificationErrors;
-//        public WebServer webServer;
+namespace Machete.Test
+{
+    [TestClass]
+    public class PersonTtests
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        public WebServer webServer;
 
-//        [TestInitialize]
-//        public void SetupTest()
-//        {
-//            //var absolutePathToWebsite = ConfigurationManager.AppSettings["absolutePathToWebsite"];
-//            var absolutePathToWebsite = @"H:\jimmy\Documents\VS2010\Projects\Machete\Machete.Web";
-//            webServer = new WebServer(absolutePathToWebsite, 42420);
-//            try
-//            {
-//                webServer.Start();
-//            }
-//            catch (Exception e)
-//            {
-//            }
-//            selenium = new DefaultSelenium("localhost", 4444, "*firefox", "http://www.google.com/");
-//            selenium.Start();
-//            verificationErrors = new StringBuilder();
-//        }
+        [TestInitialize]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://localhost/";
+            verificationErrors = new StringBuilder();
+        }
 
-//        [TestCleanup]
-//        public void TeardownTest()
-//        {
-//        try
-//        {
-//        webServer.Stop();
-//        selenium.Stop();
-//        }
-//        catch (Exception e)
-//        {
-//        // Ignore errors if unable to close the browser
-//        }
-//        Assert.AreEqual("", verificationErrors.ToString());
-//        }
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            try
+            {
+                webServer.Stop();
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
 
-//        [TestMethod]
-//        public void Se_LogonLogoff()
-//        {
-//                selenium.Open("/");
-//                    selenium.Click("link=Logon");
-//                    selenium.WaitForPageToLoad("30000");
-//                    selenium.Type("UserName", "jadmin");
-//                    selenium.Type("Password", "1bud4me");
-//                    selenium.Click("logonB");
-//                    selenium.WaitForPageToLoad("30000");
-//                    selenium.Click("link=Worker Signin");
-//                    selenium.WaitForPageToLoad("30000");
-//                    selenium.TypeKeys("dwccardentry", "12345");
-//                    selenium.Click("//input[@value='Signin?!']");
-//                    selenium.WaitForPageToLoad("30000");
-//                    try
-//                    {
-//                        Assert.IsTrue(selenium.IsTextPresent("12345"));
-//                    }
-//                    catch (Exception e)
-//                    {
-//                        verificationErrors.Append(e.Message);
-//                    }
-//                    Assert.IsTrue(selenium.IsTextPresent("Jimmy"));
-//                    selenium.TypeKeys("dwccardentry", "12345");
-//                    selenium.Click("//input[@value='Signin?!']");
-//                    selenium.WaitForPageToLoad("30000");
-//                    selenium.Click("link=Delete");
-//                    selenium.WaitForPageToLoad("30000");
-//                    Assert.IsTrue(selenium.IsTextPresent("No data available in table"));
-//                    selenium.Click("link=Logoff");
-//                    selenium.WaitForPageToLoad("30000");
-//                    Assert.IsTrue(selenium.IsTextPresent("Logon"));
-//                    Assert.IsFalse(selenium.IsTextPresent("Welcome jadmin!"));
-//        }
+        [TestMethod]
+        public void TheWebdriverTest()
+        {
+            driver.Navigate().GoToUrl("http://localhost/");
+            //
+            // Front page, logon
+            driver.FindElement(By.LinkText("Logon")).Click();
+            WaitForText("Account Information", driver, 60);
+            driver.FindElement(By.Id("UserName")).Clear();
+            driver.FindElement(By.Id("UserName")).SendKeys("jadmin");
+            driver.FindElement(By.Id("Password")).Clear();
+            driver.FindElement(By.Id("Password")).SendKeys("machete");
+            driver.FindElement(By.Name("logonB")).Click();
+            //
+            // Front page, logged in
+            WaitForText("Welcome", driver, 60);
+            WaitForElement(By.Id("menulinkperson"));
+            driver.FindElement(By.Id("menulinkperson")).Click();
+            //
+            // Persons Index page
+            WaitForElement(By.Id("personSearchBox"));
+            driver.FindElement(By.Id("personSearchBox")).Clear();
+            driver.FindElement(By.Id("personSearchBox")).SendKeys("carter");
+            //
+            // Datatables javascript update
+            WaitForElementValue(By.XPath("//table[@id='personTable']/tbody/tr/td[2]"), "Jimmy");
+            IWebElement rowrecord = driver.FindElement(By.XPath("//table[@id='personTable']/tbody/tr/td[2]"));
+            Actions actionProvider = new Actions(driver);
+            IAction doubleClick = actionProvider.DoubleClick(rowrecord).Build();
+            doubleClick.Perform();
+            //
+            // Persons edit record
+            WaitForElement(By.Id("zipcode"));
+            driver.FindElement(By.Id("zipcode")).Clear();
+            driver.FindElement(By.Id("zipcode")).SendKeys("23456");
+            driver.FindElement(By.XPath("//input[@value='Save']")).Click();
+            //
+            // Loggoff
+            WaitForElement(By.LinkText("Logoff"));
+            driver.FindElement(By.LinkText("Logoff")).Click();
+        }
+        //
+        //
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+        //
+        //
+        private bool IsElementValuePresent(By by, string value)
+        {
+            try
+            {
+                IWebElement elem = driver.FindElement(by);
+                if (elem.Text == value) return true;
+                else return false;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+        //
+        //
+        private bool WaitForElement(By by)
+        {
+            for (int second = 0; second < 60; second++)
+            {
+                try
+                {
+                    if (IsElementPresent(by)) return true;
+                }
+                catch (Exception)
+                { return false; }
+                Thread.Sleep(1000);
+            }
+            return false;
+        }
+        //
+        //
+        private bool WaitForElementValue(By by, string value)
+        {
+            for (int second = 0; second < 60; second++)
+            {
+                try
+                {
+                    if (IsElementValuePresent(by, value))
+                    {
+                        return true;
+                    }
 
-//        [TestMethod]
-//        public void TheNewTest()
-//        {
-//            // Open Google search engine.
-//            selenium.Open("http://www.google.com/");
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+                Thread.Sleep(1000);
+            }
+            return false;
+        }
 
-//            // Assert Title of page.
-//            Assert.AreEqual("Google", selenium.GetTitle());
-
-//            // Provide search term as "Selenium OpenQA"
-//            selenium.Type("q", "Selenium OpenQA");
-
-//            // Read the keyed search term and assert it.
-//            Assert.AreEqual("Selenium OpenQA", selenium.GetValue("q"));
-
-//            // Click on Search button.
-//            selenium.Click("btnG");
-
-//            // Wait for page to load.
-//            selenium.WaitForPageToLoad("5000");
-
-//            // Assert that "www.openqa.org" is available in search results.
-//            Assert.IsTrue(selenium.IsTextPresent("www.openqa.org"));
-
-//            // Assert that page title is - "Selenium OpenQA - Google Search"
-//            Assert.AreEqual("Selenium OpenQA - Google Search",
-//                         selenium.GetTitle());
-//        }
-
-//    }
-//}
-
+        private static bool isTextPresent(String what, IWebDriver driver)
+        {
+            try
+            {
+                driver.FindElement(By.XPath("//*[contains(.,'" + what + "')]"));
+                return true;
+            }
+            catch (NoSuchElementException e)
+            {
+                return false;
+            }
+        }
+        private static bool WaitForText(String what, IWebDriver driver, int waitfor)
+        {
+            for (int second = 0; second < waitfor; second++)
+            {
+                try
+                {
+                    if (isTextPresent(what, driver)) return true;
+                }
+                catch (Exception)
+                { return false; }
+                Thread.Sleep(1000);
+            }
+            return false;
+        }
+    }
+}
