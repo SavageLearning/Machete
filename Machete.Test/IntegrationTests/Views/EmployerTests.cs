3fe14466efa9438e433011aedc47b0dfc17d0f3f
Jimmy Carter <jciispam@gmail.com>
2012-01-04 05:10:33 +0000

    testing Selenium...getting out of VS11...too slow

--- Machete.Test/IntegrationTests/Views/EmployerTests.cs
+++ Machete.Test/IntegrationTests/Views/EmployerTests.cs
@@ -37,17 +37,17 @@ namespace Machete.Test
         {
             //
             // Loggoff
-            //Assert.AreEqual("", verificationErrors.ToString());
-            //ui.WaitForElement(By.LinkText("Logoff"));
-            //driver.FindElement(By.LinkText("Logoff")).Click();
-            //try
-            //{
-            //    driver.Quit();
-            //}
-            //catch (Exception)
-            //{
-            //    // Ignore errors if unable to close the browser
-            //}
+            Assert.AreEqual("", verificationErrors.ToString());
+            ui.WaitForElement(By.LinkText("Logoff"));
+            driver.FindElement(By.LinkText("Logoff")).Click();
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
 
         }
         [ClassCleanup]
