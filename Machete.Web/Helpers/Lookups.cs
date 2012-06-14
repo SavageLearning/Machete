46312d236e792c529da3979ef7d7d422382a347f
jimmy <jimmy@4c19f846-1fea-3644-b4c7-c84c78303d7d>
2012-06-14 23:42:57 +0000

    [Clean] fixed WorkerController/Create defaults Lookups Refactor: standardized dropdowns on SelectList return type mUIEXtensions: First version of mUITable objects. For Machete Editor Templates.

--- Machete.Web/Helpers/Lookups.cs
+++ Machete.Web/Helpers/Lookups.cs
@@ -18,12 +18,26 @@ namespace Machete.Web.Helpers
         public int defaultId { get; set; }
         public string name { get; set; }
         protected static IEnumerable<Lookup> DbCache { get; set; }
+        private static List<LookupGroup> groups = new List<LookupGroup>();
+
         public LookupGroup(string name)
         {
             if (name == null) throw new Exception("LookupGroup name is null");
             this.name = name;            
-            this.defaultId = getDefaultID();            
+            //this.defaultId = getDefaultID();
+            groups.Add(this);
         }
+
+        public static void Initialize(IEnumerable<Lookup> cache) 
+        {
+            DbCache = cache;
+            foreach (var a in groups)
+            {
+                a.defaultId = a.getDefaultID();
+            }
+
+        }
+
         /// <summary>
         /// Gets the default ID for the group
         /// </summary>
@@ -125,14 +139,14 @@ namespace Machete.Web.Helpers
         public static int hoursDefault { get { return 5; } }
         public static int daysDefault { get { return 1;  } }
         public static int skillLevelDefault { get { return 1; } }
-        public static double hourlyWageDefault { get { return 12; } }   
-        public static IEnumerable<LookupNumber> hours() { return hoursNum; }
-        public static IEnumerable<LookupNumber> days() { return daysNum; }
-        public static IEnumerable<LookupNumber> skillLevels() { return skillLevelNum; }
+        public static double hourlyWageDefault { get { return 12; } }
+        public static SelectList hours() { return hoursNum; }
+        public static SelectList days() { return daysNum; }
+        public static SelectList skillLevels() { return skillLevelNum; }
         private static IEnumerable<Lookup> DbCache { get; set; }
-        private static IEnumerable<LookupNumber> hoursNum { get; set; }
-        private static IEnumerable<LookupNumber> daysNum { get; set; }
-        private static IEnumerable<LookupNumber> skillLevelNum { get; set; }
+        private static SelectList hoursNum { get; set; }
+        private static SelectList daysNum { get; set; }
+        private static SelectList skillLevelNum { get; set; }
         private static List<SelectListItem> yesnoEN { get; set; }
         private static List<SelectListItem> yesnoES { get; set; }
         //
@@ -142,12 +156,18 @@ namespace Machete.Web.Helpers
         public static void Initialize()
         {
             DbCache = LookupCache.getCache();
-            hoursNum = new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16" }
-                .Select(x => new LookupNumber { Value = x, Text = x });
-            daysNum = new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14" }
-                .Select(x => new LookupNumber { Value = x, Text = x });
-            skillLevelNum = new[] {"0", "1", "2", "3", "4" }
-                .Select(x => new LookupNumber { Value = x, Text = x });        
+            LookupGroup.Initialize(DbCache);
+            hoursNum = new SelectList(new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16" }
+                .Select(x => new LookupNumber { Value = x, Text = x }),
+                "Value", "Text", "7"
+                );
+            daysNum = new SelectList(new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14" }
+                .Select(x => new LookupNumber { Value = x, Text = x }),
+                "Value", "Text", "7"
+                );            
+            skillLevelNum = new SelectList(new[] { "0", "1", "2", "3", "4" }
+                                            .Select(x => new SelectListItem { Value = x, Text = x }),
+                                            "Value", "Text", "0");
             yesnoEN = new List<SelectListItem>();
             yesnoEN.Add(new SelectListItem() { Selected = false, Text = "No", Value = "false" });
             yesnoEN.Add(new SelectListItem() { Selected = false, Text = "Yes", Value = "true" });
