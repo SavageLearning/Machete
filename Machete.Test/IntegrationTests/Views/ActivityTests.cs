90a06cfdd1aa0713117c0788a69ddfc84fcbdf98
Jimmy Carter <jciispam@gmail.com>
2012-06-19 12:13:43 -0700

    Added test and modified implementation to create data so I can write reports.

--- Machete.Test/IntegrationTests/Views/ActivityTests.cs
+++ Machete.Test/IntegrationTests/Views/ActivityTests.cs
@@ -8,6 +8,9 @@ using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Interactions;
 using Machete.Domain;
+using Machete.Data;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Machete.Test
 {
@@ -58,15 +61,35 @@ namespace Machete.Test
             ui.activityCreate(_act);
             ui.activityValidate(_act);
         }
+        [TestMethod]
+        public void SeActivity_Create_ManySignins()
+        {
+            //Arrange
+            MacheteContext DB = new MacheteContext("machete"); //maps to Machete.Text\app.config ConnectionString
+            Random rand = new Random();
+            // There's a lot in this one line. Ask questions about it.
+            //                         [DbSet] (LINQ)(Lambda Expression) (SQL-ish)
+            IEnumerable<int> list = DB.Workers.Select(q => q.dwccardnum).Distinct();
+            var count = list.Count();
+            IEnumerable<int> list1 = list.Take<int>(rand.Next(count/10));
+            Activity _act = (Activity)Records.activity.Clone();
+            ui.activityCreate(_act);
+            foreach (var i in list1)
+            {
+                ui.activitySignIn(i);
+                Thread.Sleep(2000); // cheap hack; replace with a Selenium WaitFor
+            }
 
+        }
+        
         [TestMethod]
         public void SeActivity_Create_signin()
         {
             Activity _act = (Activity)Records.activity.Clone();
             ui.activityCreate(_act);
 
-            ActivitySignin _asi = (ActivitySignin)Records.activitysignin.Clone();
-            ui.activitySignIn(_asi);
+            ActivitySignin _asi = (ActivitySignin)Records.activitysignin.Clone();            
+            ui.activitySignIn(30311);//static int is one that I know exists. replace with something smarter.
         }
     }
 }
