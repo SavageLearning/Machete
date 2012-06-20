2ef5543fc370a09327732d9b3ad5b7b6eda42871
jcii <jciispam@gmail.com>
2012-06-20 16:00:55 -0700

    Added i18n for Lookup Model (used in Config Controller)

--- Machete.Domain/Entities/Lookup.cs
+++ Machete.Domain/Entities/Lookup.cs
@@ -10,26 +10,38 @@ namespace Machete.Domain
     public class Lookup : Record
     {
         //public int ID { get; set; }
+        [LocalizedDisplayName("category", NameResourceType = typeof(Resources.Lookup))]
         [StringLength(50)]
         public string category { get; set; } //Race, Language, M-Status
+        [LocalizedDisplayName("text_EN", NameResourceType = typeof(Resources.Lookup))]
         [StringLength(50)]
         public string text_EN { get; set; }
+        [LocalizedDisplayName("text_ES", NameResourceType = typeof(Resources.Lookup))]
         [StringLength(50)]
-        public string text_ES { get; set; }    
+        public string text_ES { get; set; }
+        [LocalizedDisplayName("selected", NameResourceType = typeof(Resources.Lookup))]
         public bool selected { get; set; }
         //
         // Skill specific fields
         //
+        [LocalizedDisplayName("subcategory", NameResourceType = typeof(Resources.Lookup))]
         [StringLength(50)]
         public string subcategory { get; set; } // used in skills; allows hierarchy for skill match
+        [LocalizedDisplayName("level", NameResourceType = typeof(Resources.Lookup))]
         public int? level { get; set; }      //progression, 0 if unused
+        [LocalizedDisplayName("wage", NameResourceType = typeof(Resources.Lookup))]
         public double? wage { get; set; }
+        [LocalizedDisplayName("minHour", NameResourceType = typeof(Resources.Lookup))]
         public int? minHour { get; set; }
+        [LocalizedDisplayName("fixedJob", NameResourceType = typeof(Resources.Lookup))]
         public bool? fixedJob { get; set; }
-
+        [LocalizedDisplayName("sortorder", NameResourceType = typeof(Resources.Lookup))]
         public int sortorder { get; set; }
+        [LocalizedDisplayName("typeOfWorkID", NameResourceType = typeof(Resources.Lookup))]
         public int typeOfWorkID { get; set; } // 1 DWC, 2 HHH
+        [LocalizedDisplayName("speciality", NameResourceType = typeof(Resources.Lookup))]
         public bool speciality { get; set; }
+        [LocalizedDisplayName("ltrCode", NameResourceType = typeof(Resources.Lookup))]
         [StringLength(1)]
         public string ltrCode { get; set; }
     }
