272b2e92252a09fa9b38303bd197a773f1625e59
jcii <jciispam@gmail.com>
2012-09-13 17:54:16 -0700

    Added everytime to Machete.Web/combres.xml

---
+++ Machete.Test/UnitTests/Controllers/HelperTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AutoMapper;
+using Machete.Web.Helpers;
+
+namespace Machete.Test.UnitTests.Controllers
+{
+    [TestClass]
+    public class HelperTests
+    {
+        [TestMethod]
+        public void Call_Mapper_autoValidate()
+        {
+            MacheteMapper.Initialize();
+            Mapper.AssertConfigurationIsValid();
+        }
+    }
+}
