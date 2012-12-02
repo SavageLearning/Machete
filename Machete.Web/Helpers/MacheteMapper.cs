4145d9e3d0efcb8e7c0805584f0386aee1350b3f
jcii <jciispam@gmail.com>
2012-12-01 17:58:51 -0800

    Automapper extension IgnoreAllNonExisting -- buggy

--- Machete.Web/Helpers/MacheteMapper.cs
+++ Machete.Web/Helpers/MacheteMapper.cs
@@ -15,10 +15,10 @@ namespace Machete.Web.Helpers
         public static void Initialize()
         {
             Mapper.CreateMap<jQueryDataTableParam, viewOptions>()
-                .ForMember(vo => vo.personID, opt => opt.MapFrom(dt => dt.personID ?? 0))
                 .ForMember(vo => vo.CI, opt => opt.Ignore())
-                .ForMember(vo => vo.onlineSource, opt => opt.MapFrom(dt => string.IsNullOrEmpty(dt.searchColName("onlineSource")) ? null : dt.searchColName("onlineSource")))
                 .ForMember(vo => vo.authenticated, opt => opt.Ignore())
+                .ForMember(vo => vo.personID, opt => opt.MapFrom(dt => dt.personID ?? 0))
+                .ForMember(vo => vo.onlineSource, opt => opt.MapFrom(dt => string.IsNullOrEmpty(dt.searchColName("onlineSource")) ? null : dt.searchColName("onlineSource")))
                 .ForMember(vo => vo.status, opt => opt.MapFrom(dt => string.IsNullOrEmpty(dt.searchColName("status")) ? null : (int?)Convert.ToInt32(dt.searchColName("status"))))
                 .ForMember(vo => vo.EmployerID, opt => opt.MapFrom(dt => string.IsNullOrEmpty(dt.searchColName("EID")) ? null : (int?)Convert.ToInt32(dt.searchColName("EID"))))
                 .ForMember(vo => vo.sortColName, opt => opt.MapFrom(dt => dt.sortColName()))
@@ -31,65 +31,36 @@ namespace Machete.Web.Helpers
             #region WoCombined
             // Splitting Combined into parts
             Mapper.CreateMap<EmployerWoCombined, Employer>()
-                .ForMember(e => e.WorkOrders, opt => opt.Ignore())
-                .ForMember(e => e.active, opt => opt.Ignore())
-                .ForMember(e => e.ID, opt => opt.Ignore())
-                .ForMember(e => e.datecreated, opt => opt.Ignore())
-                .ForMember(e => e.dateupdated, opt => opt.Ignore())
-                .ForMember(e => e.Createdby, opt => opt.Ignore())
-                .ForMember(e => e.Updatedby, opt => opt.Ignore());
+                .IgnoreAllNonExisting();
             // Splitting Combined into parts
             Mapper.CreateMap<EmployerWoCombined, WorkOrder>()
-                .ForMember(wo => wo.EmployerID, opt => opt.Ignore())
-                .ForMember(wo => wo.Employer, opt => opt.Ignore())
-                .ForMember(wo => wo.workAssignments, opt => opt.Ignore())
-                .ForMember(wo => wo.workerRequests, opt => opt.Ignore())
-                .ForMember(wo => wo.paperOrderNum, opt => opt.Ignore())
-                .ForMember(wo => wo.waPseudoIDCounter, opt => opt.Ignore())
-                //.ForMember(wo => wo.contactName, opt => opt.Ignore())
-                .ForMember(wo => wo.status, opt => opt.Ignore())
-                .ForMember(wo => wo.permanentPlacement, opt => opt.Ignore())
-                .ForMember(wo => wo.transportMethodID, opt => opt.Ignore())
-                .ForMember(wo => wo.transportFee, opt => opt.Ignore())
-                .ForMember(wo => wo.transportFeeExtra, opt => opt.Ignore())
-                .ForMember(wo => wo.ID, opt => opt.Ignore())
-                .ForMember(wo => wo.datecreated, opt => opt.Ignore())
-                .ForMember(wo => wo.dateupdated, opt => opt.Ignore())
-                .ForMember(wo => wo.Createdby, opt => opt.Ignore())
-                .ForMember(wo => wo.Updatedby, opt => opt.Ignore());
+                .IgnoreAllNonExisting();
             // re-combineing to view model object
             Mapper.CreateMap<Employer, EmployerWoCombined>()
-                .ForMember(e => e.contactName, opt => opt.Ignore())
-                .ForMember(e => e.workSiteAddress1, opt => opt.Ignore())
-                .ForMember(e => e.workSiteAddress2, opt => opt.Ignore())
-                .ForMember(e => e.wo_city, opt => opt.Ignore())
-                .ForMember(e => e.wo_state, opt => opt.Ignore())
-                .ForMember(e => e.wo_phone, opt => opt.Ignore())
-                .ForMember(e => e.wo_zipcode, opt => opt.Ignore())
-                .ForMember(e => e.typeOfWorkID, opt => opt.Ignore())
-                .ForMember(e => e.englishRequired, opt => opt.Ignore())
-                .ForMember(e => e.englishRequiredNote, opt => opt.Ignore())
-                .ForMember(e => e.lunchSupplied, opt => opt.Ignore())
-                .ForMember(e => e.description, opt => opt.Ignore())
-                .ForMember(e => e.dateTimeofWork, opt => opt.Ignore())
-                .ForMember(e => e.timeFlexible, opt => opt.Ignore());
+                .IgnoreAllNonExisting();
             // re-combineing to view model object
             Mapper.CreateMap<WorkOrder, EmployerWoCombined>()
-                .ForMember(wo => wo.business, opt => opt.Ignore())
-                .ForMember(wo => wo.name, opt => opt.Ignore())
-                .ForMember(wo => wo.address1, opt => opt.Ignore())
-                .ForMember(wo => wo.address2, opt => opt.Ignore())
-                .ForMember(wo => wo.cellphone, opt => opt.Ignore())
-                .ForMember(wo => wo.email, opt => opt.Ignore())
-                .ForMember(wo => wo.referredby, opt => opt.Ignore())
-                .ForMember(wo => wo.referredbyOther, opt => opt.Ignore())
-                .ForMember(wo => wo.blogparticipate, opt => opt.Ignore())
-                .ForMember(wo => wo.notes, opt => opt.Ignore())
                 .ForMember(wo => wo.wo_city, opt => opt.MapFrom(c => c.city))
                 .ForMember(wo => wo.wo_state, opt => opt.MapFrom(c => c.state))
                 .ForMember(wo => wo.wo_phone, opt => opt.MapFrom(c => c.phone))
-                .ForMember(wo => wo.wo_zipcode, opt => opt.MapFrom(c => c.zipcode));
+                .ForMember(wo => wo.wo_zipcode, opt => opt.MapFrom(c => c.zipcode))
+                .IgnoreAllNonExisting();
             #endregion
+        }
+        // Thank you stackoverflow, allows IgnoreAllNonExisting!
+        public static IMappingExpression<TSource, TDestination> IgnoreAllNonExisting<TSource, TDestination>(
+                    this IMappingExpression<TSource, TDestination> expression)
+        {
+            var sourceType = typeof(TSource);
+            var destinationType = typeof(TDestination);
+            var existingMaps =
+                Mapper.GetAllTypeMaps().First(
+                    x => x.SourceType.Equals(sourceType) && x.DestinationType.Equals(destinationType));
+            foreach (var property in existingMaps.GetUnmappedPropertyNames())
+            {
+                expression.ForMember(property, opt => opt.Ignore());
+            }
+            return expression;
         }
     }
 }
