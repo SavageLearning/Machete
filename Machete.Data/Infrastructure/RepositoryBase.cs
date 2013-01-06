f2bf492971b2892716761c298fa88567b48bf822
Jimmy Carter <jciispam@gmail.com>
2013-01-06 14:29:04 -0800

    Added copyright notices to code

--- Machete.Data/Infrastructure/RepositoryBase.cs
+++ Machete.Data/Infrastructure/RepositoryBase.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿#region COPYRIGHT
+// File:     RepositoryBase.cs
+// Author:   Savage Learning, LLC.
+// Created:  2012/06/17 
+// License:  GPL v3
+// Project:  Machete.Data
+// Contact:  savagelearning
+// 
+// Copyright 2011 Savage Learning, LLC., all rights reserved.
+// 
+// This source file is free software, under either the GPL v3 license or a
+// BSD style license, as supplied with this software.
+// 
+// This source file is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the license files for details.
+//  
+// For details please refer to: 
+// http://www.savagelearning.com/ 
+//    or
+// http://www.github.com/jcii/machete/
+// 
+#endregion
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- Machete.Web/Scripts/jquery.macheteUI-0.1.js
+++ Machete.Web/Scripts/jquery.macheteUI-0.1.js
@@ -1,25 +1,24 @@
-/*
-* File:        jquery.macheteUI
-* Version:     0.1
-* Description: 
-* Author:      
-* Created:     
-* Language:    
-* License:     
-* Project:     
-* Contact:     
-* 
-* Copyright 2011 Savage Learning, LLC., all rights reserved.
-*
-* This source file is free software, under either the GPL v3 license or a
-* BSD style license, as supplied with this software.
-* 
-* This source file is distributed in the hope that it will be useful, but 
-* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
-* or FITNESS FOR A PARTICULAR PURPOSE. See the license files for details.
-* 
-* For details please refer to:
-*/
+// File:     juqery.macheteUI
+// Author:   Savage Learning, LLC.
+// Created:  2012/06/17 
+// License:  GPL v3
+// Project:  Machete.Web
+// Contact:  savagelearning
+// 
+// Copyright 2011 Savage Learning, LLC., all rights reserved.
+// 
+// This source file is free software, under either the GPL v3 license or a
+// BSD style license, as supplied with this software.
+// 
+// This source file is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the license files for details.
+//  
+// For details please refer to: 
+// http://www.savagelearning.com/ 
+//    or
+// http://www.github.com/jcii/machete/
+// 
 (function ($, window) {
 
     var mUI = {
