90959545595c5fd54a7b23d0ff9557ca21c1852d
jcii <jciispam@gmail.com>
2013-01-21 01:08:10 -0800

    merging colors by hand into UI. Added pngs from 1.9.2 UI...

---
+++ Machete.Test/TC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Machete.Test
+{
+    public class TC
+    {
+        public const string IT = "IntegrationTest";
+        public const string UT = "UnitTest";
+        public const string SE = "SeleniumTest";
+        //
+        //
+        public const string Service = "ServiceLayer";
+        public const string Controller = "ControllerLayer";
+        public const string View = "ViewLayer";
+        //
+        //
+        public const string WorkOrders = "WorkOrdersArea";
+        public const string Activities = "ActivitiesArea";
+        public const string Employers = "EmployersArea";
+        public const string Persons = "PersonsArea";
+        public const string WSIs = "WorkerSigninsArea";
+    }
+}
