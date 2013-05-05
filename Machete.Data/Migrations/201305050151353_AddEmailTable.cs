23e2d1d662301b0ee4960b94d1276ad9e1c42243
jcii <jciispam@gmail.com>
2013-05-04 20:40:36 -0700

    Added Entity Framework Migration code and SQL script for Email Table

---
+++ Machete.Data/Migrations/201305050151353_AddEmailTable.cs
@@ -0,0 +1,58 @@
+namespace Machete.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddEmailTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.JoinWorkorderEmails",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        WorkOrderID = c.Int(nullable: false),
+                        EmailID = c.Int(nullable: false),
+                        datecreated = c.DateTime(nullable: false),
+                        dateupdated = c.DateTime(nullable: false),
+                        Createdby = c.String(maxLength: 30),
+                        Updatedby = c.String(maxLength: 30),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.WorkOrders", t => t.WorkOrderID, cascadeDelete: true)
+                .ForeignKey("dbo.Emails", t => t.EmailID, cascadeDelete: true)
+                .Index(t => t.WorkOrderID)
+                .Index(t => t.EmailID);
+            
+            CreateTable(
+                "dbo.Emails",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        emailFrom = c.String(nullable: false, maxLength: 50),
+                        emailTo = c.String(nullable: false, maxLength: 50),
+                        subject = c.String(nullable: false, maxLength: 100),
+                        body = c.String(nullable: false, maxLength: 50),
+                        transmitAttempts = c.Int(nullable: false),
+                        lastAttempt = c.DateTime(),
+                        datecreated = c.DateTime(nullable: false),
+                        dateupdated = c.DateTime(nullable: false),
+                        Createdby = c.String(maxLength: 30),
+                        Updatedby = c.String(maxLength: 30),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.JoinWorkorderEmails", new[] { "EmailID" });
+            DropIndex("dbo.JoinWorkorderEmails", new[] { "WorkOrderID" });
+            DropForeignKey("dbo.JoinWorkorderEmails", "EmailID", "dbo.Emails");
+            DropForeignKey("dbo.JoinWorkorderEmails", "WorkOrderID", "dbo.WorkOrders");
+            DropTable("dbo.Emails");
+            DropTable("dbo.JoinWorkorderEmails");
+        }
+    }
+}
