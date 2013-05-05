275cfa22d4abbcd53b1242fbc87ca1b05629a23b
Jimmy Carter <jciispam@gmail.com>
2013-05-04 20:42:46 -0700

    Added Email object to Fluent testing harness.

--- Machete.Test/Records.cs
+++ Machete.Test/Records.cs
@@ -34,8 +34,6 @@ namespace Machete.Test
 {
     public class Records
     {
-        #region Workers
-
         public static int GetNextMemberID(DbSet<Worker> db)
         {
             var next = 10001;
@@ -103,8 +101,6 @@ namespace Machete.Test
             dateOfMembership = DateTime.Now,
             memberStatus = MacheteLookup.cache.First(x => x.category == "memberstatus" && x.text_EN == "Active").ID
         };
-        #endregion
-        #region Persons
         public static Person person = new Person
         {
             firstname1 = "barack",
@@ -123,8 +119,6 @@ namespace Machete.Test
             Updatedby = "TestInitializer"
         };
 
-        #endregion
-        #region Events
         public static Event event1 = new Event
         {
             Createdby = "TestInitializer",
@@ -136,7 +130,18 @@ namespace Machete.Test
             eventType = MacheteLookup.cache.First(x => x.category == "eventtype" && x.text_EN == "Complaint").ID,
             notes = "Remember Sammy Jenkis"
         };
-        #endregion
+
+        public static Email email = new Email
+        {
+            emailTo = "joe@boo.com",
+            emailFrom = "boo@joe.com",
+            subject = "subject",
+            body = "foo",
+            datecreated = DateTime.Now,             //datetime
+            dateupdated = DateTime.Now,              //datetime
+            Createdby = "TestInitializer",
+            Updatedby = "TestInitializer"
+        };
         public static Employer employer = new Employer
         {
             name = "Willy Wonka",
@@ -252,99 +257,5 @@ namespace Machete.Test
             Updatedby = "TestInitializer"
         };
 
-
-
-        public static void Initialize(MacheteContext DB)
-        {
-            var dt = DateTime.Today;
-            #region INIT persons and workers
-
-            Person p1 = (Person)person.Clone(); p1.Worker = (Worker)worker.Clone(); p1.Worker.dwccardnum = 30040; p1.Worker.skill1 = 62;
-            p1.Worker.Person = p1;
-            DB.Persons.Add(p1);
-            DB.Workers.Add(p1.Worker);
-            Person p2 = (Person)person.Clone(); DB.Persons.Add(p2); p2.Worker = (Worker)worker.Clone(); p2.Worker.dwccardnum = 30041;
-            Person p3 = (Person)person.Clone(); DB.Persons.Add(p3); p3.Worker = (Worker)worker.Clone(); p3.Worker.dwccardnum = 30042;
-
-            //DB.SaveChanges();
-            WorkerSignin wsi1 = (WorkerSignin)signin.Clone(); DB.WorkerSignins.Add(wsi1); wsi1.dwccardnum = 30040; wsi1.dateforsignin = dt; wsi1.WorkerID = 1;
-            WorkerSignin wsi2 = (WorkerSignin)signin.Clone(); DB.WorkerSignins.Add(wsi2); wsi2.dwccardnum = 30041; wsi2.dateforsignin = dt; wsi2.WorkerID = 2;
-            WorkerSignin wsi3 = (WorkerSignin)signin.Clone(); DB.WorkerSignins.Add(wsi3); wsi3.dwccardnum = 30042; wsi3.dateforsignin = dt; wsi3.WorkerID = 3;
-            WorkerSignin wsi4 = (WorkerSignin)signin.Clone(); DB.WorkerSignins.Add(wsi4); wsi4.dwccardnum = 30043; wsi4.dateforsignin = dt;
-            WorkerSignin wsi5 = (WorkerSignin)signin.Clone(); DB.WorkerSignins.Add(wsi5); wsi5.dwccardnum = 30044; wsi5.dateforsignin = dt;
-            DB.SaveChanges();
-            #endregion
-            #region INIT employers
-            Employer em1 = (Employer)employer.Clone(); DB.Employers.Add(em1);
-            Employer em2 = (Employer)employer.Clone(); DB.Employers.Add(em2);
-            Employer em3 = (Employer)employer.Clone(); DB.Employers.Add(em3);
-            DB.SaveChanges();
-            #endregion
-            #region INIT workorders
-            WorkOrder em1od1 = (WorkOrder)order.Clone();
-            em1od1.Employer = em1;
-            em1od1.paperOrderNum = 12420;
-            em1od1.contactName = "Umpa1";
-            em1od1.status = 42;            
-            DB.WorkOrders.Add(em1od1); //CreateWorkOrder
-            DB.SaveChanges();
-            WorkOrder em2od1 = (WorkOrder)order.Clone(); em2od1.Employer = em2; em2od1.paperOrderNum = 12421; em2od1.contactName = "Umpa2"; em2od1.status = 43; DB.WorkOrders.Add(em2od1);
-            WorkOrder em2od2 = (WorkOrder)order.Clone(); em2od2.Employer = em2; em2od2.paperOrderNum = 12422; em2od2.contactName = "Umpa3"; em2od2.status = 44; DB.WorkOrders.Add(em2od2);
-            WorkOrder em3od1 = (WorkOrder)order.Clone(); em3od1.Employer = em3; em3od1.paperOrderNum = 12423; em3od1.contactName = "Umpa4"; em3od1.status = 45; DB.WorkOrders.Add(em3od1);
-            WorkOrder em3od2 = (WorkOrder)order.Clone(); em3od2.Employer = em3; em3od2.paperOrderNum = 12424; em3od2.contactName = "Umpa5"; em3od2.status = 44; DB.WorkOrders.Add(em3od2);
-            WorkOrder em3od3 = (WorkOrder)order.Clone(); em3od3.Employer = em3; em3od3.paperOrderNum = 12425; em3od3.contactName = "Umpa6"; em3od3.status = 42; DB.WorkOrders.Add(em3od3);
-            DB.SaveChanges();
-            #endregion
-            #region INIT workassignments
-            WorkAssignment em1od1as1 = (WorkAssignment)assignment.Clone(); em1od1as1.workOrder = em1od1; DB.WorkAssignments.Add(em1od1as1);
-            em1od1as1.description = "foostring1";
-            em1od1as1.Updatedby = "foostring2";
-            em1od1as1.workerAssignedID = 1;
-            em1od1as1.skillID = 70; DB.WorkAssignments.Add(em1od1as1);
-
-            em1od1.dateTimeofWork = DateTime.Parse(DateTime.Today.ToShortDateString() + " 09:00:00");
-            WorkAssignment em1od1as2 = (WorkAssignment)assignment.Clone(); em1od1as2.workOrder = em1od1; DB.WorkAssignments.Add(em1od1as2);
-            em1od1as2.workerAssignedID = 2;
-            em1od1as2.skillID = 61;
-            em1od1as2.englishLevelID = 2;
-            WorkAssignment em1od1as3 = (WorkAssignment)assignment.Clone(); em1od1as3.workOrder = em1od1; DB.WorkAssignments.Add(em1od1as3);
-            em1od1as1.workerAssignedID = 1;
-            em1od1as3.workerAssignedID = 3;
-            //
-            WorkAssignment em2od1as1 = (WorkAssignment)assignment.Clone(); em2od1as1.workOrder = em2od1; DB.WorkAssignments.Add(em2od1as1);
-            em2od1as1.workerAssignedID = 1;
-            WorkAssignment em2od1as2 = (WorkAssignment)assignment.Clone(); em2od1as2.workOrder = em2od1; DB.WorkAssignments.Add(em2od1as2);
-            em2od1as2.workerAssignedID = 2;
-            //
-            WorkAssignment em2od2as1 = (WorkAssignment)assignment.Clone(); em2od2as1.workOrder = em2od2; DB.WorkAssignments.Add(em2od2as1);
-            em2od2as1.workerAssignedID = 1;
-            WorkAssignment em2od2as2 = (WorkAssignment)assignment.Clone(); em2od2as2.workOrder = em2od2; DB.WorkAssignments.Add(em2od2as2);
-            em2od2as2.workerAssignedID = 2;
-            //
-            WorkAssignment em3od1as1 = (WorkAssignment)assignment.Clone(); em3od1as1.workOrder = em3od1; DB.WorkAssignments.Add(em3od1as1);
-            em3od1as1.workerAssignedID = 1;
-            //
-            WorkAssignment em3od2as1 = (WorkAssignment)assignment.Clone(); em3od2as1.workOrder = em3od2; DB.WorkAssignments.Add(em3od2as1);
-            em3od2as1.workerAssignedID = 1;
-            //
-            WorkAssignment em3od3as1 = (WorkAssignment)assignment.Clone(); em3od3as1.workOrder = em3od3; DB.WorkAssignments.Add(em3od3as1);
-            WorkerRequest od3wr1 = (WorkerRequest)request.Clone();
-            em3od3.workerRequests = new Collection<WorkerRequest>();
-            od3wr1.workerRequested = p2.Worker;
-            od3wr1.workOrder = em3od3;
-
-
-            em3od3.workerRequests.Add(od3wr1);
-            DB.SaveChanges(); 
-            #endregion
-            #region INIT ACTIVITIES
-            Activity a1 = (Activity)activity.Clone(); DB.Activities.Add(a1);
-            Activity a2 = (Activity)activity.Clone(); DB.Activities.Add(a2);
-            Activity a3 = (Activity)activity.Clone(); DB.Activities.Add(a3);
-            DB.SaveChanges();
-            ActivitySignin a1as1 = (ActivitySignin)activitysignin.Clone(); DB.ActivitySignins.Add(a1as1); a1as1.activityID = 1; a1as1.dwccardnum = 30040; a1as1.dateforsignin = dt; a1as1.personID = 1;
-            DB.SaveChanges();
-            #endregion
-        }
     }
 }
