595598d1c0ffaeaa95ab791f60962a3e1fc78f37
Jimmy Carter <jciispam@gmail.com>
2013-05-04 17:26:05 -0700

    Added EmailService; extends ServiceBase for Email. Inherits CRUD methods; this file will contain only Email-specific service methods. Added 2 properties to Email domain

---
+++ Machete.Service/EmailService.cs
@@ -0,0 +1,22 @@
+﻿using Machete.Data;
+using Machete.Data.Infrastructure;
+using Machete.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Machete.Service
+{
+    public interface IEmailService : IService<Email>
+    {
+    }
+
+    public class EmailService : ServiceBase<Email>, IEmailService
+    {
+        public EmailService(IEmailRepository emRepo, IUnitOfWork uow) : base(emRepo, uow)
+        {
+            this.logPrefix = "Email";
+        }
+    }
+}
