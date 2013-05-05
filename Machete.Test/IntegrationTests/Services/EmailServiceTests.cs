92f1e87695aaf5006dc89b3f175127080fa5cd65
jcii <jciispam@gmail.com>
2013-05-04 20:43:13 -0700

    First successful write to Email db from MacheteService layer

---
+++ Machete.Test/IntegrationTests/Services/EmailServiceTests.cs
@@ -0,0 +1,80 @@
+﻿#region COPYRIGHT
+// File:     EmailServiceTests.cs
+// Author:   Savage Learning, LLC.
+// Created:  2013/05/04
+// License:  GPL v3
+// Project:  Machete.Test
+// Contact:  savagelearning
+// 
+// Copyright 2011 Savage Learning, LLC., all rights reserved.
+// 
+// This source file is free software, under either the GPL v3 license or a
+// BSD style license, as supplied with this software.
+// 
+// This source file is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the license files for details.
+//  
+// For details please refer to: 
+// http://www.savagelearning.com/ 
+//    or
+// http://www.github.com/jcii/machete/
+// 
+#endregion
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Machete.Domain;
+using Machete.Data;
+using System.Data.Entity;
+using Machete.Service;
+using Machete.Data.Infrastructure;
+using System.Globalization;
+namespace Machete.Test.IntegrationTests.Services
+{
+    [TestClass]
+    public class EmailServiceTests
+    {
+        viewOptions dOptions;
+        FluentRecordBase frb;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            frb = new FluentRecordBase();
+            frb.Initialize(new MacheteInitializer(), "macheteConnection");
+            LookupCache.Initialize(frb.DB);
+            dOptions = new viewOptions
+            {
+                CI = new CultureInfo("en-US", false),
+                sSearch = "",
+                date = DateTime.Today,
+                dwccardnum = null,
+                woid = null,
+                orderDescending = false,
+                sortColName = "",
+                displayStart = 0,
+                displayLength = 20
+            };
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            frb.Dispose();
+            frb = null;
+        }
+
+        [TestMethod]
+        public void Integration_Activity_Service_CreateEmail()
+        {
+            //Arrange
+            //Act
+            var result = frb.ToEmail();
+            //Assert
+            Assert.IsNotNull(result.ID, "Email.ID is null");
+        }
+    }
+}
