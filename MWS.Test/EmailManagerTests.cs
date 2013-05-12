5a2a912ab1233da9571a804ac6afcee0c28f57b2
jcii <jciispam@gmail.com>
2013-05-12 16:50:09 -0700

    Added projects for Machete Windows Service (MWS).

---
+++ MWS.Test/EmailManagerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MWS.Test
+{
+    [TestClass]
+    public class EmailManagerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
