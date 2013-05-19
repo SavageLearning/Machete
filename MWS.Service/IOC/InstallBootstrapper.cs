f1af023e186e91d543f28fc565f03921baad786e
jcii <jciispam@gmail.com>
2013-05-19 15:10:06 -0700

    Combined code from ProjectInstaller.Designer and ProjectInstaller. Going to implement Unity Container and want the code in one file.

---
+++ MWS.Service/IOC/InstallBootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MWS.Service.IOC
+{
+    internal class InstallBootstrapper
+    {
+        public IUnityContainer Build()
+        {
+            IUnityContainer container = new UnityContainer();
+            return container;
+        }
+    }
+}
