283a99140e45e3039ab3aae0c1e0a6ebd8bacbb6
Jimmy Carter <jciispam@gmail.com>
2013-05-19 17:07:01 -0700

    Switched out ProjectInstaller's dependencies with Unity provided DI

--- MWS.Service/IOC/InstallBootstrapper.cs
+++ MWS.Service/IOC/InstallBootstrapper.cs
@@ -1,17 +1,20 @@
 ﻿using Microsoft.Practices.Unity;
 using System;
 using System.Collections.Generic;
+using System.Configuration.Install;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MWS.Service.IOC
+namespace MWS.Service
 {
-    internal class InstallBootstrapper
+    public class InstallBootstrapper
     {
         public IUnityContainer Build()
         {
             IUnityContainer container = new UnityContainer();
+            container.RegisterType<Installer, MacheteServiceInstaller>("Installer", new ContainerControlledLifetimeManager())
+                     .RegisterType<Installer, MacheteServiceProcessInstaller>("ProcessInstaller", new ContainerControlledLifetimeManager());
             return container;
         }
     }
