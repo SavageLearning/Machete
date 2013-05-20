d8fdf681cb72fa1f41f3ae321bfffce0e6aebf05
jcii <jciispam@gmail.com>
2013-05-19 17:07:01 -0700

    Switched out ProjectInstaller's dependencies with Unity provided DI

---
+++ MWS.Service/IOC/MacheteServiceProcessInstaller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MWS.Service
+{
+    /// <summary>
+    /// Extension to contain Machete defaults
+    /// </summary>
+    internal class MacheteServiceProcessInstaller : ServiceProcessInstaller
+    {
+        public MacheteServiceProcessInstaller() : base()
+        {
+            Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            Password = null;
+            Username = null;
+        }
+    }
+}
