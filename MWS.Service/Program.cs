b8234cc77871928859c8498de028aa6455d7736e
jcii <jciispam@gmail.com>
2013-06-16 12:40:16 -0700

    Added easier method to debug service from blog: http://coding.abel.nu/2012/05/debugging-a-windows-service-project/

--- MWS.Service/Program.cs
+++ MWS.Service/Program.cs
@@ -3,8 +3,10 @@ using MWS.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.ServiceProcess;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MWS.Service
@@ -22,7 +24,51 @@ namespace MWS.Service
             {
                 container.Resolve<MacheteWindowsService>()
             };
-            ServiceBase.Run(services);
+            //ServiceBase.Run(services);
+            if (Environment.UserInteractive)
+            {
+                RunInteractive(services);
+            }
+            else
+            {
+                ServiceBase.Run(services);
+            }
+        }
+        //
+        // http://coding.abel.nu/2012/05/debugging-a-windows-service-project/
+        static void RunInteractive(ServiceBase[] servicesToRun)
+        {
+            Console.WriteLine("Services running in interactive mode.");
+            Console.WriteLine();
+
+            MethodInfo onStartMethod = typeof(ServiceBase).GetMethod("OnStart",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+            foreach (ServiceBase service in servicesToRun)
+            {
+                Console.Write("Starting {0}...", service.ServiceName);
+                onStartMethod.Invoke(service, new object[] { new string[] { } });
+                Console.Write("Started");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(
+                "Press any key to stop the services and end the process...");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            MethodInfo onStopMethod = typeof(ServiceBase).GetMethod("OnStop",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+            foreach (ServiceBase service in servicesToRun)
+            {
+                Console.Write("Stopping {0}...", service.ServiceName);
+                onStopMethod.Invoke(service, null);
+                Console.WriteLine("Stopped");
+            }
+
+            Console.WriteLine("All services stopped.");
+            // Keep the console alive for a second to allow the user to see the message.
+            Thread.Sleep(1000);
         }
     }
 }
