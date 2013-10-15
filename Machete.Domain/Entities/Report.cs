1ac5e379d2e88cc68e6ccc5ce03c6fd031a102b2
Chaim Eliyah <chaimeliyah@gmail.com>
2013-10-14 17:59:45 -0700

    Fixed.

--- Machete.Domain/Entities/Report.cs
+++ Machete.Domain/Entities/Report.cs
@@ -22,20 +22,6 @@ namespace Machete.Domain.Entities
         public double avg { get; set; }
     }
 
-    public class MonthlyWithDetailReport
-    {
-        /// A class containing all of the data for the Monthly Report with Details
-        public DateTime date { get; set; }
-        public int totalSignins { get; set; }
-        public int totalDWCSignins { get; set; }
-        public int totalHHHSignins { get; set; }
-        public int dispatchedDWCSignins { get; set; }
-        public int dispatchedHHHSignins { get; set; }
-        public int totalHours { get; set; }
-        public double totalIncome { get; set; }
-    }
-
-
     public class reportUnit
     {
         public DateTime? date { get; set; }
