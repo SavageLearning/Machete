aea6373309942f74c1108c19c407b6af5691433c
Chaim Eliyah <chaimeliyah@gmail.com>
2013-10-25 16:13:09 -0700

    Fixed spreadsheets, did some controller editing, working on print views.

--- Machete.Web/ViewModel/ReportPrintView.cs
+++ Machete.Web/ViewModel/ReportPrintView.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using Machete.Service;
+
 namespace Machete.Web.ViewModel
 {
     public class ReportPrintView<T>
     {
-        public IEnumerable<T> report { get; set; }
+        public dataTableResult<T> report { get; set; }
     }
 }
