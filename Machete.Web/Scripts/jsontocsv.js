a526723ef6a98ce4871d7ae5cd4e8f7919cb29d8
Chaim Eliyah <chaimeliyah@gmail.com>
2013-10-25 00:29:33 -0700

    Worst commit yet. Struggling with JavaScript. Need more server side support for making files, etc.--will work on that before this update goes out. BLAH.

---
+++ Machete.Web/Scripts/jsontocsv.js
@@ -0,0 +1,32 @@
+//////////////JSONTOCSV//////////////
+//
+// JSON2CSV
+// Credit and thanks: http://stackoverflow.com/users/64741/zachary
+function JSON2CSV(objArray) {
+    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+
+    var str = '';
+    var line = '';
+    var head = array[0];
+
+    for (var index in array[0]) {
+        line += index + ',';
+    }
+
+    line = line.slice(0, -1);
+    str += line + '\r\n';
+
+    for (var i = 0; i < array.length; i++) {
+        var line = '';
+
+        for (var index in array[i]) {
+            line += array[i][index] + ',';
+        }
+
+        line = line.slice(0, -1);
+        str += line + '\r\n';
+
+    }
+
+    return str;
+};
