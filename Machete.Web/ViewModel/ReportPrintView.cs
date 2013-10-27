16a7bc7432f6dc2d9534a1320369e3dd9629a9a3
Chaim Eliyah <chaimeliyah@gmail.com>
2013-10-26 17:03:17 -0700

    EVERYTHING working on print view besides actual HTML, which I haven't written.

--- Machete.Web/ViewModel/ReportPrintView.cs
+++ Machete.Web/ViewModel/ReportPrintView.cs
@@ -7,8 +7,29 @@ using Machete.Service;
 
 namespace Machete.Web.ViewModel
 {
-    public class ReportPrintView<T>
+    public class ReportPrintView
     {
-        public dataTableResult<T> report { get; set; }
+        public string typeOfReport { get; set; }
+        public DateTime date { get; set; }
+    }
+
+    public class DailyReportPrintView : ReportPrintView
+    {
+        public dataTableResult<dailyData> report { get; set; }
+    }
+
+    public class WeeklyReportPrintView : ReportPrintView
+    {
+        public dataTableResult<weeklyData> report { get; set; }
+    }
+
+    public class MonthlyReportPrintView : ReportPrintView
+    {
+        public dataTableResult<monthlyData> report { get; set; }
+    }
+
+    public class JzcReportPrintView : ReportPrintView
+    {
+        public dataTableResult<jzcData> report { get; set; }
     }
 }
