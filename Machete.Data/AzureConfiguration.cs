23a08c07b3f316c9d5dc8892ceb0221ecccf1798
chaim1221 <chaimeliyah@gmail.com>
2014-01-06 02:27:40 -0800

    Implemented EF6 logging.... Didn't help.

---
+++ Machete.Data/AzureConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Machete.Data.Logging;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Data.Entity.SqlServer;
+
+namespace Machete.Data
+{
+    class AzureConfiguration : DbConfiguration
+    {
+        public AzureConfiguration()
+        {
+            SetExecutionStrategy(
+                "System.Data.SqlClient", 
+                () => new SqlAzureExecutionStrategy(1, TimeSpan.FromSeconds(30)));
+            DbInterception.Add(new MacheteInterceptorLogging());
+        }
+    }
+}
