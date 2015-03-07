7469966502d08475c15604e7eeca54a92f3862c3
Melanie A. Davis <melanie.davis@gmail.com>
2015-03-07 00:44:35 -0800

    Hydrating lookup tables with new entries

--- Machete.Data/Migrations/201412212342567_AddTransportTransactionData.cs
+++ Machete.Data/Migrations/201412212342567_AddTransportTransactionData.cs
@@ -9,9 +9,6 @@ namespace Machete.Data
         {
             AddColumn("dbo.WorkOrders", "transportTransactType", c => c.Int());
             AddColumn("dbo.WorkOrders", "transportTransactID", c => c.String(maxLength: 50));
-            Sql("SET IDENTITY_INSERT dbo.Lookups ON;");
-            Sql("INSERT INTO Lookups(ID, category, text_EN, text_ES, selected, speciality, dateCreated, dateUpdated, createdBy, updatedBy) VALUES(255, 'transportPaymentType','Cash','Cash', 1, 0, getDate(), getDate(), 'DB.Migration', 'DB.Migration'), (256, 'transportPaymentType','Paypal','Paypal', 0, 0, getDate(), getDate(), 'DB.Migration', 'DB.Migration'), (257, 'transportPaymentType','Check','Check', 0, 0, getDate(), getDate(), 'DB.Migration', 'DB.Migration'), (258, 'transportPaymentType','Credit Card','Credit Card', 0, 0, getDate(), getDate(), 'DB.Migration', 'DB.Migration');");
-            Sql("SET IDENTITY_INSERT dbo.Lookups OFF;");
         }
         
         public override void Down()
@@ -20,8 +17,6 @@ namespace Machete.Data
             DropColumn("dbo.WorkOrders", "transportTransactID");
             // ALTER TABLE dbo.WorkOrders DROP COLUMN transportTransactType;
             DropColumn("dbo.WorkOrders", "transportTransactType");
-            // DELETE FROM Lookups WHERE category='transportPaymentType';
-            Sql("DELETE FROM Lookups WHERE category='transportPaymentType';");
         }
 
         
