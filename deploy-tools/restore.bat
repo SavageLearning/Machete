ac16c2d41122825b4f61f297610a2df42095576e
Melanie A. Davis <melanie.davis@gmail.com>
2015-03-07 13:53:49 -0800

    Adding script-tools to help automate parts of deployment

---
+++ deploy-tools/restore.bat
@@ -0,0 +1,20 @@
+REM this scrips exists in c:\temp directory on macheteprod.cloudapp.net
+
+copy C:\temp\test.connections.config C:\inetpub\wwwroot\machete-test\connections.config 
+copy C:\temp\alabama.connections.config C:\inetpub\wwwroot\machete-alabama\connections.config 
+copy C:\temp\casa.connections.config C:\inetpub\wwwroot\machete-casa\connections.config
+copy C:\temp\centrohumanitario.connections.config C:\inetpub\wwwroot\machete-centrohumanitario\connections.config 
+copy C:\temp\concord.connections.config C:\inetpub\wwwroot\machete-concord\connections.config 
+copy C:\temp\congreso.connections.config C:\inetpub\wwwroot\machete-congreso\connections.config 
+copy C:\temp\donbosco.connections.config C:\inetpub\wwwroot\machete-donbosco\connections.config
+copy C:\temp\elcentro.connections.config C:\inetpub\wwwroot\machete-elcentro\connections.config 
+copy C:\temp\graton.connections.config C:\inetpub\wwwroot\machete-graton\connections.config 
+copy C:\temp\mtnview.connections.config C:\inetpub\wwwroot\machete-mtnview\connections.config 
+copy C:\temp\nice.connections.config C:\inetpub\wwwroot\machete-nice\connections.config 
+copy C:\temp\pasadena.connections.config C:\inetpub\wwwroot\machete-pasadena\connections.config 
+copy C:\temp\pomona.connections.config C:\inetpub\wwwroot\machete-pomona\connections.config 
+copy C:\temp\sanfrancisco.connections.config C:\inetpub\wwwroot\machete-sanfrancisco\connections.config
+copy C:\temp\santacruz.connections.config C:\inetpub\wwwroot\machete-santacruz\connections.config 
+copy C:\temp\southside.connections.config C:\inetpub\wwwroot\machete-southside\connections.config 
+copy C:\temp\voz.connections.config C:\inetpub\wwwroot\machete-voz\connections.config 
+copy C:\temp\wjp.connections.config C:\inetpub\wwwroot\machete-wjp\connections.config 
