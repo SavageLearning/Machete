89fd43de48b13d852264ca1895c57be1d9796deb
Jimmy Carter <jciispam@gmail.com>
2016-11-20 10:16:59 -0800

    config to return json by default

--- Machete.Web/App_Start/WebApiConfig.cs
+++ Machete.Web/App_Start/WebApiConfig.cs
@@ -1,9 +1,12 @@
-﻿using System.Web.Http;
+﻿using System.Net.Http.Headers;
+using System.Web.Http;
 
 class WebApiConfig
 {
     public static void Register(HttpConfiguration configuration)
     {
+        configuration.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
         configuration.Routes.MapHttpRoute("API Default", "api/{controller}/{id}",
             new { id = RouteParameter.Optional });
     }
