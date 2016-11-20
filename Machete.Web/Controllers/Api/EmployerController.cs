64bd32cfe9ff619f27b38a41c7f0afe35e0f2e2f
Jimmy Carter <jciispam@gmail.com>
2016-11-19 16:32:25 -0800

    Added Elmah and EmployerService injection

--- Machete.Web/Controllers/Api/EmployerController.cs
+++ Machete.Web/Controllers/Api/EmployerController.cs
@@ -1,16 +1,29 @@
-﻿using System;
+﻿using Machete.Service;
+using Machete.Web.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace Machete.Api.Controllers
 {
+    [ElmahHandleError]
     [System.Web.Http.Authorize]
     [RoutePrefix("api/employer")]
     public class EmployerController : ApiController
     {
+        private readonly IEmployerService serv;
+        private readonly IWorkOrderService woServ;
+        private System.Globalization.CultureInfo CI;
+
+        public EmployerController(IEmployerService employerService, IWorkOrderService workorderService)
+        {
+            this.serv = employerService;
+            this.woServ = workorderService;
+        }
         // GET api/values
         public IEnumerable<string> Get()
         {
