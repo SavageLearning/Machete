fbedce86fd86bb1aae239e3323cb034cf480fb74
jcii <jciispam@gmail.com>
2017-05-28 11:07:00 -0700

    adding scaffolding for auth

--- Machete.Web/App_Start/IdentityConfig.cs
+++ Machete.Web/App_Start/IdentityConfig.cs
@@ -1,11 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using wepabi_auth_scaffolding.Models;
 
-namespace Machete.Web.App_Start
+namespace wepabi_auth_scaffolding
 {
-    public class IdentityConfig
-    {
-    }
+    // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
+
+    //public class ApplicationUserManager : UserManager<Machete.Data.ApplicationUser>
+    //{
+    //    public ApplicationUserManager(IUserStore<Machete.Data.ApplicationUser> store)
+    //        : base(store)
+    //    {
+    //    }
+
+    //    public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
+    //    {
+    //        var manager = new ApplicationUserManager(new UserStore<Machete.Data.ApplicationUser>(context.Get<ApplicationDbContext>()));
+    //        // Configure validation logic for usernames
+    //        manager.UserValidator = new UserValidator<Machete.Data.ApplicationUser>(manager)
+    //        {
+    //            AllowOnlyAlphanumericUserNames = false,
+    //            RequireUniqueEmail = true
+    //        };
+    //        // Configure validation logic for passwords
+    //        manager.PasswordValidator = new PasswordValidator
+    //        {
+    //            RequiredLength = 6,
+    //            RequireNonLetterOrDigit = true,
+    //            RequireDigit = true,
+    //            RequireLowercase = true,
+    //            RequireUppercase = true,
+    //        };
+    //        var dataProtectionProvider = options.DataProtectionProvider;
+    //        if (dataProtectionProvider != null)
+    //        {
+    //            manager.UserTokenProvider = new DataProtectorTokenProvider<Machete.Data.ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
+    //        }
+    //        return manager;
+    //    }
+    //}
 }
