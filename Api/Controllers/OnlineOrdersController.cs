fad3802e2ef6b15feb769bfd76f862d728472e36
jcii <jciispam@gmail.com>
2017-12-25 19:57:21 -0800

    refactored paypalexecute to use HttpExceptionResponse

--- Api/Controllers/OnlineOrdersController.cs
+++ Api/Controllers/OnlineOrdersController.cs
@@ -12,6 +12,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Security.Claims;
+using System.Threading;
 using System.Web.Http;
 
 namespace Machete.Api.Controllers
@@ -46,6 +47,7 @@ namespace Machete.Api.Controllers
             this.paypalSecret = cServ.getConfig(Cfg.PaypalSecret);
             this.paypalUrl = cServ.getConfig(Cfg.PaypalUrl);
         }
+
         // GET: api/OnlineOrders
         [ClaimsAuthorization(ClaimType = CAType.Role, ClaimValue = new[] { CV.Admin, CV.Employer })]
 
@@ -95,14 +97,117 @@ namespace Machete.Api.Controllers
         [ClaimsAuthorization(ClaimType = CAType.Role, ClaimValue = new[] { CV.Admin, CV.Employer })]
         public IHttpActionResult PaypalExecute(int orderID, [FromBody]PaypalPayment data)
         {
-            var token = getPaypalAuthToken(paypalUrl, paypalId, paypalSecret);
+            validatePaypalData(data);
+
+            var employer = eServ.Get(userSubject);
+            validateEmployer(employer);
+
+            var order = serv.GetMany(o => o.ID == orderID && o.EmployerID == employer.ID).First();
+            validateOrder(order, employer);
+
+            validateNoPreviousPayment(order, data);
+
+            if (order.ppState == null)
+            {
+                order.ppPayerID = data.payerID;
+                order.ppPaymentID = data.paymentID;
+                order.ppPaymentToken = data.paymentToken;
+                order.ppState = "created";
+                woServ.Save(order, User.Identity.Name);
+            }
+
+            var result = postExecute(data);
+            return Json(result);
+        }
+
+        [NonAction]
+        public void validateNoPreviousPayment(Domain.WorkOrder wo, PaypalPayment pp)
+        {
+            if (wo.ppPayerID != null && wo.ppPayerID != pp.payerID)
+            {
+                var res = new HttpResponseMessage(HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent(string.Format("PaypalID already set to {0}, conflicts with {1}", pp.payerID, wo.ppPayerID)),
+                    ReasonPhrase = "PaypalID already set to a different ID"
+                };
+                throw new HttpResponseException(res);
+            }
+            if (wo.ppPaymentID != null && wo.ppPaymentID != pp.paymentID)
+            {
+                var res = new HttpResponseMessage(HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent(string.Format("PaymentID already set to {0}, conflicts with {1}", pp.paymentID, wo.ppPaymentID)),
+                    ReasonPhrase = "PaymentID already set to a different ID"
+                };
+                throw new HttpResponseException(res);
+            }
+
+            if (wo.ppPaymentToken != null && wo.ppPaymentToken != pp.paymentToken)
+            {
+                var res = new HttpResponseMessage(HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent(string.Format("PaymentToken already set to {0}, conflicts with {1}", pp.paymentToken, wo.ppPaymentToken)),
+                    ReasonPhrase = "PaymentToken already set to a different ID"
+                };
+                throw new HttpResponseException(res);
+            }
+        }
+
+        [NonAction]
+        public void validateOrder(Domain.WorkOrder wo, Domain.Employer e)
+        {
+            if (wo == null)
+            {
+                var res = new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                    Content = new StringContent(string.Format("Order {0} not found for employer {1}", wo.ID, e.ID)),
+                    ReasonPhrase = "Workorder not found"
+                };
+                throw new HttpResponseException(res);
+            }
+        }
+
+        [NonAction]
+        public void validatePaypalData(PaypalPayment pp)
+        {
+            if (pp.payerID == null || pp.paymentID == null || pp.paymentToken == null)
+            {
+                var res = new HttpResponseMessage(HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent(string.Format("Paypal data: {0}", JsonConvert.SerializeObject(pp))),
+                    ReasonPhrase = "Incomplete Paypal data"
+                };
+                throw new HttpResponseException(res);
+            }
+        }
+
+        [NonAction]
+        public void validateEmployer(Domain.Employer e)
+        {
+            if (e == null)
+            {
+                var res = new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                    Content = new StringContent(string.Format("No employer for IdentitySubject: {0}", userSubject)),
+                    ReasonPhrase = "Employer not found for user's IdentitySubject"
+                };
+                throw new HttpResponseException(res);
+            }
+        }
+
+        [NonAction]
+        public string postExecute(PaypalPayment data)
+        {
+            // paypal info loaded from database, set at controller creation
+            var auth = getPaypalAuthToken(paypalUrl, paypalId, paypalSecret);
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-            var client = new RestClient("https://api.sandbox.paypal.com/v1/payments/payment/"+data.paymentID+"/execute");
+
+            var client = new RestClient(paypalUrl + "/payments/payment/" + data.paymentID + "/execute");
             var request = new RestRequest(Method.POST) { RequestFormat = DataFormat.Json };
 
-            request.AddHeader("Authorization", string.Format("bearer {0}", token.access_token));
+            request.AddHeader("Authorization", string.Format("bearer {0}", auth.access_token));
             request.AddHeader("Accept", "application/json");
-            request.AddParameter("application/json", "{ \"payer_id\": \""+ data.payerID +"\"}", ParameterType.RequestBody);
+            request.AddParameter("application/json", "{ \"payer_id\": \"" + data.payerID + "\"}", ParameterType.RequestBody);
 
             var result = client.Execute(request);
             if (result.StatusCode != HttpStatusCode.OK)
@@ -110,21 +215,35 @@ namespace Machete.Api.Controllers
                 throw result.ErrorException;
             }
 
-            return Ok(result.Content);
+            return result.Content;
         }
 
         [NonAction]
         public PayPalTokenModel getPaypalAuthToken(string url, string clientId, string clientSecret)
         {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-            var client = new RestClient(url);
-            var request = new RestRequest(Method.POST) { RequestFormat = DataFormat.Json };
+            var client = new RestClient(url + "/oauth2/token");
             client.Authenticator = new HttpBasicAuthenticator(clientId, clientSecret);
 
+            var request = new RestRequest(Method.POST) { RequestFormat = DataFormat.Json };
             request.AddParameter("grant_type", "client_credentials");
-            IRestResponse response = client.Execute(request);
-            var payPalTokenModel = JsonConvert.DeserializeObject<PayPalTokenModel>(response.Content);
-            return payPalTokenModel;
+
+            IRestResponse response;
+            for (int i = 0; i<5; i++)
+            {
+                response = client.Execute(request);
+                if (response.IsSuccessful)
+                {
+                    var payPalTokenModel = JsonConvert.DeserializeObject<PayPalTokenModel>(response.Content);
+                    return payPalTokenModel;
+                }
+                Thread.Sleep(1000);
+            }
+            var res = new HttpResponseMessage(HttpStatusCode.GatewayTimeout)
+            {
+                ReasonPhrase = "Failed to retrieve access token from Paypal"
+            };
+            throw new HttpResponseException(res);
         }
     }
 
@@ -132,6 +251,7 @@ namespace Machete.Api.Controllers
     {
         public string payerID;
         public string paymentID;
+        public string paymentToken;
 
     }
 
