b307b124fba63721389f158f1c418dc37e3e85ef
Chaim Eliyah <chaimeliyah@gmail.com>
2017-12-29 15:20:09 -0800

    adding IReadOnlyContext to DI container

--- Machete.Web/Global.asax.cs
+++ Machete.Web/Global.asax.cs
@@ -112,6 +112,7 @@ namespace Machete.Web
             .RegisterType<IUserStore<MacheteUser>, UserStore<MacheteUser>>(new PerResolveLifetimeManager())//HttpContextLifetimeManager<IUserStore<ApplicationUser>>())
             .RegisterType<IMacheteUserManager<MacheteUser>, MacheteUserManager>(new PerResolveLifetimeManager())//HttpContextLifetimeManager<IMyUserManager<ApplicationUser>>())
             .RegisterType<IDatabaseFactory, DatabaseFactory>(new PerResolveLifetimeManager(), new InjectionConstructor("macheteConnection"))
+            .RegisterType<IReadOnlyContext, ReadOnlyContext>(new PerResolveLifetimeManager(), new InjectionConstructor("readOnlyConnection"))
             .RegisterType<IUnitOfWork, UnitOfWork>(new PerResolveLifetimeManager())
             .RegisterType<IEmailConfig, EmailConfig>(new PerResolveLifetimeManager())
             .RegisterInstance<IMapper>(new MapperConfig().getMapper())
