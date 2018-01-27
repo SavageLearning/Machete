08dbafdd4a02b18a49be4ddc7e273744e9c7c572
jcii <jciispam@gmail.com>
2018-01-26 21:22:46 -0800

    Designer xml noise

--- Machete.Web/App_Start/Startup.Auth.cs
+++ Machete.Web/App_Start/Startup.Auth.cs
@@ -19,22 +19,6 @@ namespace Machete.Web
             });
             // Use a cookie to temporarily store information about a user logging in with a third party login provider
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
-
-            // Uncomment the following lines to enable logging in with third party login providers
-
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
-
-            //app.UseTwitterAuthentication(
-            //   consumerKey: "",
-            //   consumerSecret: "");
-
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
-
-            //app.UseGoogleAuthentication();
         }
     }
 }
