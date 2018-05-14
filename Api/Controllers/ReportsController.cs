49a61750776d4e1e8fb4d54c712ebd118a82ef02
Chaim Eliyah <chaimeliyah@gmail.com>
2018-05-14 09:23:26 -0700

    clean build

--- Api/Controllers/ReportsController.cs
+++ Api/Controllers/ReportsController.cs
@@ -86,7 +86,7 @@ namespace Machete.Api.Controllers
             } catch (Exception ex) {
                 // SQL errors are expected but they will be returned as strings (200).
                 // in this case, something happened that we were not expecting; return 500.
-                var message = ex.Message;
+                var message = ex.Message; 
                 var statusCode = HttpStatusCode.InternalServerError;
                 var error = Request.CreateErrorResponse(statusCode, message);
                 return ResponseMessage(error);
