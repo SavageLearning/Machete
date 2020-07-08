ecbecec438285d3e7ccfc7a25d4b313dbd2b5ef8
esteban-gs <comsat61@gmail.com>
2020-07-08 00:14:48 -0700

    improved WOSummary view, batch print by date timezones fixed

--- Machete.Data/Repositories/AllRepositories.cs
+++ Machete.Data/Repositories/AllRepositories.cs
@@ -169,13 +169,12 @@ namespace Machete.Data
         
         public IEnumerable<WorkOrder> GetActiveOrders(DateTime date, TimeZoneInfo clientTimeZoneInfo)
         {
-            // date parameter comes in as Utc datetime, so convert before comparing
-            var clientDateUTC = TimeZoneInfo.ConvertTimeToUtc(DateTime.SpecifyKind(date, DateTimeKind.Unspecified), clientTimeZoneInfo); 
-            var nextDateUTC = clientDateUTC.AddDays(1);
+           // date parameter comes in as Utc datetime, e.g 6/29/20 + some offset hour
+            var dateEndUtc = date.AddHours(24); // UTC end search dateTime
             return dbset.Where(wo => 
                 wo.statusID == WorkOrder.iActive &&
-                wo.dateTimeofWork > clientDateUTC &&
-                wo.dateTimeofWork < nextDateUTC
+                wo.dateTimeofWork >= date &&
+                wo.dateTimeofWork < dateEndUtc
             ).Include(a => a.Employer)
                 .Include(a => a.workerRequestsDDD)
                 .ThenInclude(a => a.workerRequested)
