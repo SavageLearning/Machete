b36b3eaae27e2b0598a02cf8eb7504b2bfd168cf
jcii <jciispam@gmail.com>
2021-05-15 16:52:48 -0700

    missed the new version of the script

--- appveyor_deploy.sh
+++ appveyor_deploy.sh
@@ -2,10 +2,16 @@
 set -ex
 
 cd Machete.Web && dotnet publish -o published && cd ..
-if [ $APPVEYOR_REPO_BRANCH == 'master' ]; then docker build -t ndlonmachete/debian:$APPVEYOR_BUILD_VERSION .; fi
-if [ $APPVEYOR_REPO_BRANCH != 'master' ]; then docker build -t ndlonmachete/debian:$APPVEYOR_BUILD_VERSION-beta .; fi
-docker images
-docker login -u chaim1221 -p "$DOCKER_PASSWORD"
-if [ $APPVEYOR_REPO_BRANCH == 'master' ]; then docker push ndlonmachete/debian:$APPVEYOR_BUILD_VERSION; fi
-if [ $APPVEYOR_REPO_BRANCH != 'master' ]; then docker push ndlonmachete/debian:$APPVEYOR_BUILD_VERSION-beta; fi
-if [ $APPVEYOR_REPO_BRANCH == 'master' ]; then cd github && npm install && node github.js create-machete-release && cd ..; fi  
+if [ $APPVEYOR_REPO_BRANCH == 'master' ]; then 
+  docker build -t ndlonmachete/debian:$APPVEYOR_BUILD_VERSION .
+  docker images
+  docker login -u chaim1221 -p "$DOCKER_PASSWORD"
+  docker push ndlonmachete/debian:$APPVEYOR_BUILD_VERSION
+  cd github && npm install && node github.js create-machete-release && cd ..
+fi
+if [ $APPVEYOR_REPO_BRANCH != 'master' ]; then
+  docker build -t ndlonmachete/debian:$APPVEYOR_BUILD_VERSION-beta .
+  docker images
+  docker login -u chaim1221 -p "$DOCKER_PASSWORD"
+  docker push ndlonmachete/debian:$APPVEYOR_BUILD_VERSION-beta
+fi
