0dfb88b0286b24cae1b18cd448e4b875d96ba610
jcii <jciispam@gmail.com>
2021-05-15 17:17:05 -0700

    remove duplicate run

--- appveyor_prebuild.sh
+++ appveyor_prebuild.sh
@@ -7,7 +7,6 @@ dotnet clean
 cd UI
 npm install
 npm run --silent build-prod
-npm run build-prod 
 cd ..
 sed -i "s/APPVEYOR_VERSION/$APPVEYOR_BUILD_VERSION/" "./Machete.Web/Views/Home/Index.cshtml"
   
