a207cee7ac4e76fb49869ff7f33dadd92ad7d90a
jcii <jciispam@gmail.com>
2021-05-23 22:07:07 -0700

    changing npm install to local

--- appveyor_install.sh
+++ appveyor_install.sh
@@ -3,6 +3,6 @@ set -ex
 
 git submodule init
 git submodule update
-npm install -g @angular/cli
+npm install @angular/cli
 dotnet restore
 #docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=passw0rD!' --network machete-bridge -p 1433:1433 --name sqlserver -d mcr.microsoft.com/mssql/server
