c785866d011baae080f24c371e73e54722f95b2a
jcii <jciispam@gmail.com>
2021-05-23 22:27:30 -0700

    trying service name as hsot name

--- appveyor_install.sh
+++ appveyor_install.sh
@@ -6,5 +6,5 @@ set -ex
 #npm install @angular/cli
 dotnet restore
 docker ps -a
-docker inspect mssql
+docker network ls
 #docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=passw0rD!' --network machete-bridge -p 1433:1433 --name sqlserver -d mcr.microsoft.com/mssql/server
