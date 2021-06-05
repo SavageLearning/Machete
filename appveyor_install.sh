af463ff857780d182ec2f9e13fd32f9f4521ee50
jcii <jciispam@gmail.com>
2021-06-05 00:43:55 -0700

    build & publish from github actions

--- appveyor_install.sh
+++ appveyor_install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 set -ex
 
-#git submodule init
-#git submodule update
-#npm install @angular/cli
+npm install @angular/cli
 dotnet restore
-docker ps -a
-docker network ls
-#docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=passw0rD!' --network machete-bridge -p 1433:1433 --name sqlserver -d mcr.microsoft.com/mssql/server
